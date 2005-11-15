7de0470d72b116ccfa3a4270ac92994d667df120
Brian McCallister <brianm@skife.org>
2005-11-15 02:22:17 +0000

    Start adding better exception handling

---
+++ src/test/org/skife/jdbi/TestExceptionHandling.java
@@ -0,0 +1,14 @@
+package org.skife.jdbi;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class TestExceptionHandling extends TestCase
+{
+    public void testNothingYet() throws Exception
+    {
+        
+    }
+}
