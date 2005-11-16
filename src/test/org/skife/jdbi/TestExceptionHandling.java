d62e81c38db980ac643d326b2538dde6c18c2948
Brian McCallister <brianm@skife.org>
2005-11-16 23:47:59 +0000

    Store SQLException when thrown in order to provide to DBIException for better exception messages, etc

--- src/test/org/skife/jdbi/TestExceptionHandling.java
+++ src/test/org/skife/jdbi/TestExceptionHandling.java
@@ -1,14 +1,49 @@
+/* Copyright 2004-2005 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.skife.jdbi;
 
 import junit.framework.TestCase;
+import org.skife.jdbi.derby.Tools;
 
-/**
- * 
- */
 public class TestExceptionHandling extends TestCase
 {
-    public void testNothingYet() throws Exception
+    private Handle handle;
+
+    public void setUp() throws Exception
     {
-        
+        Tools.start();
+        Tools.dropAndCreateSomething();
+        this.handle = DBI.open(Tools.CONN_STRING);
+    }
+
+    public void tearDown() throws Exception
+    {
+        Tools.stop();
+    }
+
+    public void testSomething() throws Exception
+    {
+        try
+        {
+            handle.query("select wombats from something");
+            fail("should throw an exception");
+        }
+        catch (DBIException e)
+        {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
     }
 }
