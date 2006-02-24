571c5280f67f76f77550c5c7c389cacc7a0e10c2
Brian McCallister <brianm@skife.org>
2006-02-24 18:16:07 +0000

    Add some jdbc annotation magic testing

---
+++ src/test/org/skife/jdbi/v2/TestAnnotationMagic.java
@@ -0,0 +1,34 @@
+package org.skife.jdbi.v2;
+
+import junit.framework.TestCase;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * 
+ */
+public class TestAnnotationMagic extends TestCase
+{
+    public void testSomething() throws Exception
+    {
+        Method m = Wombat.class.getMethod("m");
+        Type return_type = m.getGenericReturnType();
+        ParameterizedType parameterized_return_type = (ParameterizedType) return_type;
+        Type parameterized_type_on_return_type = parameterized_return_type.getActualTypeArguments()[0];
+        Class we_require_this_be_instantiable = (Class) parameterized_type_on_return_type;
+        Objsect it = we_require_this_be_instantiable.newInstance();
+        assertTrue(it instanceof String);
+    }
+
+    public static class Wombat
+    {
+        public List<String> m()
+        {
+            return null;
+        }
+    }
+}
+
