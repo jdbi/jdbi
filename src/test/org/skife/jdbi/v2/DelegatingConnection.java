ca0e596c961156bc7b8a7454cb1025949e449e7a
Brian McCallister <brianm@skife.org>
2006-03-06 04:30:38 +0000

    Test for the prepared statement cache

---
+++ src/test/org/skife/jdbi/v2/DelegatingConnection.java
@@ -0,0 +1,215 @@
+/* Copyright 2004-2006 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.skife.jdbi.v2;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLWarning;
+import java.sql.Savepoint;
+import java.util.Map;
+
+public class DelegatingConnection implements Connection
+{
+    private Connection connection;
+
+    public DelegatingConnection(Connection delegate)
+    {
+        this.connection = delegate;
+    }
+
+    public Statement createStatement() throws SQLException
+    {
+        return connection.createStatement();
+    }
+
+    public PreparedStatement prepareStatement(String s) throws SQLException
+    {
+        return connection.prepareStatement(s);
+    }
+
+    public CallableStatement prepareCall(String s) throws SQLException
+    {
+        return connection.prepareCall(s);
+    }
+
+    public String nativeSQL(String s) throws SQLException
+    {
+        return connection.nativeSQL(s);
+    }
+
+    public void setAutoCommit(boolean b) throws SQLException
+    {
+        connection.setAutoCommit(b);
+    }
+
+    public boolean getAutoCommit() throws SQLException
+    {
+        return connection.getAutoCommit();
+    }
+
+    public void commit() throws SQLException
+    {
+        connection.commit();
+    }
+
+    public void rollback() throws SQLException
+    {
+        connection.rollback();
+    }
+
+    public void close() throws SQLException
+    {
+        connection.close();
+    }
+
+    public boolean isClosed() throws SQLException
+    {
+        return connection.isClosed();
+    }
+
+    public DatabaseMetaData getMetaData() throws SQLException
+    {
+        return connection.getMetaData();
+    }
+
+    public void setReadOnly(boolean b) throws SQLException
+    {
+        connection.setReadOnly(b);
+    }
+
+    public boolean isReadOnly() throws SQLException
+    {
+        return connection.isReadOnly();
+    }
+
+    public void setCatalog(String s) throws SQLException
+    {
+        connection.setCatalog(s);
+    }
+
+    public String getCatalog() throws SQLException
+    {
+        return connection.getCatalog();
+    }
+
+    public void setTransactionIsolation(int i) throws SQLException
+    {
+        connection.setTransactionIsolation(i);
+    }
+
+    public int getTransactionIsolation() throws SQLException
+    {
+        return connection.getTransactionIsolation();
+    }
+
+    public SQLWarning getWarnings() throws SQLException
+    {
+        return connection.getWarnings();
+    }
+
+    public void clearWarnings() throws SQLException
+    {
+        connection.clearWarnings();
+    }
+
+    public Statement createStatement(int i, int i1) throws SQLException
+    {
+        return connection.createStatement(i, i1);
+    }
+
+    public PreparedStatement prepareStatement(String s, int i, int i1) throws SQLException
+    {
+        return connection.prepareStatement(s, i, i1);
+    }
+
+    public CallableStatement prepareCall(String s, int i, int i1) throws SQLException
+    {
+        return connection.prepareCall(s, i, i1);
+    }
+
+    public Map<String, Class<?>> getTypeMap() throws SQLException
+    {
+        return connection.getTypeMap();
+    }
+
+    public void setTypeMap(Map<String, Class<?>> map) throws SQLException
+    {
+        connection.setTypeMap(map);
+    }
+
+    public void setHoldability(int i) throws SQLException
+    {
+        connection.setHoldability(i);
+    }
+
+    public int getHoldability() throws SQLException
+    {
+        return connection.getHoldability();
+    }
+
+    public Savepoint setSavepoint() throws SQLException
+    {
+        return connection.setSavepoint();
+    }
+
+    public Savepoint setSavepoint(String s) throws SQLException
+    {
+        return connection.setSavepoint(s);
+    }
+
+    public void rollback(Savepoint savepoint) throws SQLException
+    {
+        connection.rollback(savepoint);
+    }
+
+    public void releaseSavepoint(Savepoint savepoint) throws SQLException
+    {
+        connection.releaseSavepoint(savepoint);
+    }
+
+    public Statement createStatement(int i, int i1, int i2) throws SQLException
+    {
+        return connection.createStatement(i, i1, i2);
+    }
+
+    public PreparedStatement prepareStatement(String s, int i, int i1, int i2) throws SQLException
+    {
+        return connection.prepareStatement(s, i, i1, i2);
+    }
+
+    public CallableStatement prepareCall(String s, int i, int i1, int i2) throws SQLException
+    {
+        return connection.prepareCall(s, i, i1, i2);
+    }
+
+    public PreparedStatement prepareStatement(String s, int i) throws SQLException
+    {
+        return connection.prepareStatement(s, i);
+    }
+
+    public PreparedStatement prepareStatement(String s, int[] ints) throws SQLException
+    {
+        return connection.prepareStatement(s, ints);
+    }
+
+    public PreparedStatement prepareStatement(String s, String[] strings) throws SQLException
+    {
+        return connection.prepareStatement(s, strings);
+    }
+}
---
+++ src/test/org/skife/jdbi/v2/TestPreparedStatementCache.java
@@ -0,0 +1,57 @@
+/* Copyright 2004-2006 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.skife.jdbi.v2;
+
+import org.skife.jdbi.derby.Tools;
+import org.skife.jdbi.v2.tweak.transactions.LocalTransactionHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TestPreparedStatementCache extends DBITestCase
+{
+    public void testSomething() throws Exception
+    {
+        final int[] prep_count = { 0 };
+
+        Connection c = new DelegatingConnection(Tools.getConnection())
+        {
+            public PreparedStatement prepareStatement(String s) throws SQLException
+            {
+                prep_count[0]++;
+                return super.prepareStatement(s);
+            }
+        };
+        PreparedStatementCache cache = new PreparedStatementCache(c);
+
+        BasicHandle h = new BasicHandle(new LocalTransactionHandler(),
+                                        cache,
+                                        new NamedParameterStatementRewriter(),
+                                        c);
+
+        h.createStatement("insert into something (id, name) values (:id, :name)")
+                .bindFromProperties(new Something(0, "Keith"))
+                .execute();
+
+        assertEquals(1, prep_count[0]);
+
+        h.createStatement("insert into something (id, name) values (:id, :name)")
+                .bindFromProperties(new Something(0, "Keith"))
+                .execute();
+        
+        assertEquals(1, prep_count[0]);
+    }
+}
