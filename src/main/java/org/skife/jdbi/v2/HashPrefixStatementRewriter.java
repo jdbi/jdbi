9170109b9e6f760e308806481ba73bf0e893ba64
Brian McCallister <brianm@skife.org>
2011-05-08 19:37:11 -0600

    hash prefix statement rewriter and rewriter override on sql objects

--- src/main/java/org/skife/jdbi/v2/HashPrefixStatementRewriter.java
+++ src/main/java/org/skife/jdbi/v2/HashPrefixStatementRewriter.java
@@ -1,51 +1,47 @@
 /*
  * Copyright 2004 - 2011 Brian McCallister
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package org.skife.jdbi.v2;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.Token;
+import org.skife.jdbi.rewriter.hash.HashStatementLexer;
 import org.skife.jdbi.v2.exceptions.UnableToCreateStatementException;
 import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
 import org.skife.jdbi.v2.tweak.Argument;
 import org.skife.jdbi.v2.tweak.RewrittenStatement;
 import org.skife.jdbi.v2.tweak.StatementRewriter;
-import org.skife.jdbi.rewriter.colon.ColonStatementLexer;
-
-import static org.skife.jdbi.rewriter.colon.ColonStatementLexer.*;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Statement rewriter which replaces named parameter tokens of the form :tokenName
- * <p/>
- * This is the default statement rewriter
- */
-public class ColonPrefixNamedParamStatementRewriter implements StatementRewriter
+import static org.skife.jdbi.rewriter.hash.HashStatementLexer.*;
+
+public class HashPrefixStatementRewriter implements StatementRewriter
 {
     /**
      * Munge up the SQL as desired. Responsible for figuring out ow to bind any
      * arguments in to the resultant prepared statement.
      *
-     * @param sql The SQL to rewrite
+     * @param sql    The SQL to rewrite
      * @param params contains the arguments which have been bound to this statement.
-     * @param ctx The statement context for the statement being executed
+     * @param ctx    The statement context for the statement being executed
+     *
      * @return somethign which can provde the actual SQL to prepare a statement from
      *         and which can bind the correct arguments to that prepared statement
      */
@@ -65,30 +61,30 @@ public class ColonPrefixNamedParamStatementRewriter implements StatementRewriter
     String parseString(final String sql, final ParsedStatement stmt) throws IllegalArgumentException
     {
         StringBuilder b = new StringBuilder();
-        ColonStatementLexer lexer = new ColonStatementLexer(new ANTLRStringStream(sql));
+        HashStatementLexer lexer = new HashStatementLexer(new ANTLRStringStream(sql));
         Token t = lexer.nextToken();
-        while (t.getType() != ColonStatementLexer.EOF) {
+        while (t.getType() != HashStatementLexer.EOF) {
             switch (t.getType()) {
-            case LITERAL:
-                b.append(t.getText());
-                break;
-            case NAMED_PARAM:
-                stmt.addNamedParamAt(t.getText().substring(1, t.getText().length()));
-                b.append("?");
-                break;
-            case QUOTED_TEXT:
-                b.append(t.getText());
-                break;
-            case DOUBLE_QUOTED_TEXT:
-                b.append(t.getText());
-                break;
-            case POSITIONAL_PARAM:
-                b.append("?");
-                stmt.addPositionalParamAt();
-                break;
-            case ESCAPED_TEXT:
-                b.append(t.getText().substring(1));
-                break;
+                case LITERAL:
+                    b.append(t.getText());
+                    break;
+                case NAMED_PARAM:
+                    stmt.addNamedParamAt(t.getText().substring(1, t.getText().length()));
+                    b.append("?");
+                    break;
+                case QUOTED_TEXT:
+                    b.append(t.getText());
+                    break;
+                case DOUBLE_QUOTED_TEXT:
+                    b.append(t.getText());
+                    break;
+                case POSITIONAL_PARAM:
+                    b.append("?");
+                    stmt.addPositionalParamAt();
+                    break;
+                case ESCAPED_TEXT:
+                    b.append(t.getText().substring(1));
+                    break;
             }
             t = lexer.nextToken();
         }
@@ -117,12 +113,12 @@ public class ColonPrefixNamedParamStatementRewriter implements StatementRewriter
                     final Argument a = params.forPosition(i);
                     if (a != null) {
                         try {
-                        a.apply(i + 1, statement, this.context);
+                            a.apply(i + 1, statement, this.context);
                         }
                         catch (SQLException e) {
                             throw new UnableToExecuteStatementException(
-                                    String.format("Exception while binding positional param at (0 based) position %d",
-                                                  i), e, context);
+                                String.format("Exception while binding positional param at (0 based) position %d",
+                                              i), e, context);
                         }
                     }
                     else {
