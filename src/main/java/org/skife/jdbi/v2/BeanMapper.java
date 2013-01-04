b5fc0f5f1c0b3751d6c43303d115439b5648fb97
Brian McCallister <brianm@skife.org>
2013-01-04 14:55:16 -0700

    get rid of the tabs

--- src/main/java/org/skife/jdbi/v2/BeanMapper.java
+++ src/main/java/org/skife/jdbi/v2/BeanMapper.java
@@ -40,126 +40,117 @@ import java.util.Map;
 public class BeanMapper<T> implements ResultSetMapper<T>
 {
     private final Class<T> type;
-	private final Map<String, PropertyDescriptor> properties = new HashMap<String, PropertyDescriptor>();
+    private final Map<String, PropertyDescriptor> properties = new HashMap<String, PropertyDescriptor>();
 
-	public BeanMapper(Class<T> type)
+    public BeanMapper(Class<T> type)
     {
         this.type = type;
-        try
-        {
+        try {
             BeanInfo info = Introspector.getBeanInfo(type);
 
-			for (PropertyDescriptor descriptor : info.getPropertyDescriptors())
-			{
-				properties.put(descriptor.getName().toLowerCase(), descriptor);
-			}
-		}
-        catch (IntrospectionException e)
-        {
+            for (PropertyDescriptor descriptor : info.getPropertyDescriptors()) {
+                properties.put(descriptor.getName().toLowerCase(), descriptor);
+            }
+        }
+        catch (IntrospectionException e) {
             throw new IllegalArgumentException(e);
         }
-	}
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-  public T map(int row, ResultSet rs, StatementContext ctx)
-			throws SQLException
-	{
-		T bean;
-        try
-        {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public T map(int row, ResultSet rs, StatementContext ctx)
+        throws SQLException
+    {
+        T bean;
+        try {
             bean = type.newInstance();
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             throw new IllegalArgumentException(String.format("A bean, %s, was mapped " +
                                                              "which was not instantiable", type.getName()),
                                                e);
         }
 
-		ResultSetMetaData metadata = rs.getMetaData();
+        ResultSetMetaData metadata = rs.getMetaData();
 
-		for (int i = 1; i <= metadata.getColumnCount(); ++i) {
-			String name = metadata.getColumnLabel(i).toLowerCase();
+        for (int i = 1; i <= metadata.getColumnCount(); ++i) {
+            String name = metadata.getColumnLabel(i).toLowerCase();
 
-			PropertyDescriptor descriptor = properties.get(name);
+            PropertyDescriptor descriptor = properties.get(name);
 
-			if (descriptor != null) {
-				Class<?> type = descriptor.getPropertyType();
+            if (descriptor != null) {
+                Class<?> type = descriptor.getPropertyType();
 
-				Object value;
+                Object value;
 
-				if (type.isAssignableFrom(Boolean.class) || type.isAssignableFrom(boolean.class)) {
-					value = rs.getBoolean(i);
-				}
-				else if (type.isAssignableFrom(Byte.class) || type.isAssignableFrom(byte.class)) {
-					value = rs.getByte(i);
-				}
-				else if (type.isAssignableFrom(Short.class) || type.isAssignableFrom(short.class)) {
-					value = rs.getShort(i);
-				}
-				else if (type.isAssignableFrom(Integer.class) || type.isAssignableFrom(int.class)) {
-					value = rs.getInt(i);
-				}
-				else if (type.isAssignableFrom(Long.class) || type.isAssignableFrom(long.class)) {
-					value = rs.getLong(i);
-				}
-				else if (type.isAssignableFrom(Float.class) || type.isAssignableFrom(float.class)) {
-					value = rs.getFloat(i);
-				}
-				else if (type.isAssignableFrom(Double.class) || type.isAssignableFrom(double.class)) {
-					value = rs.getDouble(i);
-				}
-				else if (type.isAssignableFrom(BigDecimal.class)) {
-					value = rs.getBigDecimal(i);
-				}
-				else if (type.isAssignableFrom(Timestamp.class)) {
-					value = rs.getTimestamp(i);
-				}
-				else if (type.isAssignableFrom(Time.class)) {
-					value = rs.getTime(i);
-				}
-				else if (type.isAssignableFrom(Date.class)) {
-					value = rs.getDate(i);
-				}
-				else if (type.isAssignableFrom(String.class)) {
-					value = rs.getString(i);
-				}
-				else {
-					value = rs.getObject(i);
-				}
+                if (type.isAssignableFrom(Boolean.class) || type.isAssignableFrom(boolean.class)) {
+                    value = rs.getBoolean(i);
+                }
+                else if (type.isAssignableFrom(Byte.class) || type.isAssignableFrom(byte.class)) {
+                    value = rs.getByte(i);
+                }
+                else if (type.isAssignableFrom(Short.class) || type.isAssignableFrom(short.class)) {
+                    value = rs.getShort(i);
+                }
+                else if (type.isAssignableFrom(Integer.class) || type.isAssignableFrom(int.class)) {
+                    value = rs.getInt(i);
+                }
+                else if (type.isAssignableFrom(Long.class) || type.isAssignableFrom(long.class)) {
+                    value = rs.getLong(i);
+                }
+                else if (type.isAssignableFrom(Float.class) || type.isAssignableFrom(float.class)) {
+                    value = rs.getFloat(i);
+                }
+                else if (type.isAssignableFrom(Double.class) || type.isAssignableFrom(double.class)) {
+                    value = rs.getDouble(i);
+                }
+                else if (type.isAssignableFrom(BigDecimal.class)) {
+                    value = rs.getBigDecimal(i);
+                }
+                else if (type.isAssignableFrom(Timestamp.class)) {
+                    value = rs.getTimestamp(i);
+                }
+                else if (type.isAssignableFrom(Time.class)) {
+                    value = rs.getTime(i);
+                }
+                else if (type.isAssignableFrom(Date.class)) {
+                    value = rs.getDate(i);
+                }
+                else if (type.isAssignableFrom(String.class)) {
+                    value = rs.getString(i);
+                }
+                else {
+                    value = rs.getObject(i);
+                }
 
-				if (rs.wasNull() && !type.isPrimitive()) {
-					value = null;
-				}
-				
-        if (type.isEnum() && value != null) {
-          value = Enum.valueOf((Class) type, (String) value);
+                if (rs.wasNull() && !type.isPrimitive()) {
+                    value = null;
+                }
+
+                if (type.isEnum() && value != null) {
+                    value = Enum.valueOf((Class) type, (String) value);
+                }
+
+                try {
+                    descriptor.getWriteMethod().invoke(bean, value);
+                }
+                catch (IllegalAccessException e) {
+                    throw new IllegalArgumentException(String.format("Unable to access setter for " +
+                                                                     "property, %s", name), e);
+                }
+                catch (InvocationTargetException e) {
+                    throw new IllegalArgumentException(String.format("Invocation target exception trying to " +
+                                                                     "invoker setter for the %s property", name), e);
+                }
+                catch (NullPointerException e) {
+                    throw new IllegalArgumentException(String.format("No appropriate method to " +
+                                                                     "write value %s ", value.toString()), e);
+                }
+
+            }
         }
-
-				try
-				{
-					descriptor.getWriteMethod().invoke(bean, value);
-				}
-				catch (IllegalAccessException e)
-				{
-					throw new IllegalArgumentException(String.format("Unable to access setter for " +
-																	 "property, %s", name), e);
-				}
-				catch (InvocationTargetException e)
-				{
-					throw new IllegalArgumentException(String.format("Invocation target exception trying to " +
-																	 "invoker setter for the %s property", name), e);
-				}
-        catch (NullPointerException e)
-        {
-          throw new IllegalArgumentException(String.format("No appropriate method to " +
-                                   "write value %s ", value.toString()), e);
-        }
-
-			}
-		}
 
         return bean;
-	}
+    }
 }
 
