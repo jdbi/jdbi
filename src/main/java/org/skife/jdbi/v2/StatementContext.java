965de7b75ae4b3d64adb8109ad08e29a5591df38
Henning Schmiedehausen <henning@schmiedehausen.org>
2013-09-23 23:47:10 -0700

    fix license headers

--- src/main/java/org/skife/jdbi/v2/StatementContext.java
+++ src/main/java/org/skife/jdbi/v2/StatementContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004 - 2011 Brian McCallister
+ * Copyright (C) 2004 - 2013 Brian McCallister
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.skife.jdbi.v2;
 
 import java.lang.reflect.Method;
