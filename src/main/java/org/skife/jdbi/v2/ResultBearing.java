92f1fad175a6faae748ca8199513feffdfedf49e
Henning Schmiedehausen <henning@schmiedehausen.org>
2014-01-22 21:37:02 -0800

    remove a few checkstyle warnings

--- src/main/java/org/skife/jdbi/v2/ResultBearing.java
+++ src/main/java/org/skife/jdbi/v2/ResultBearing.java
@@ -19,10 +19,10 @@ import java.util.List;
 
 public interface ResultBearing<ResultType> extends Iterable<ResultType>
 {
-    public <ContainerType> ContainerType list(Class<ContainerType> containerType);
-    public List<ResultType> list(final int maxRows);
-    public List<ResultType> list();
-    public ResultIterator<ResultType> iterator();
-    public ResultType first();
-    public <T> T first(Class<T> containerType);
+    <ContainerType> ContainerType list(Class<ContainerType> containerType);
+    List<ResultType> list(final int maxRows);
+    List<ResultType> list();
+    ResultIterator<ResultType> iterator();
+    ResultType first();
+    <T> T first(Class<T> containerType);
 }
