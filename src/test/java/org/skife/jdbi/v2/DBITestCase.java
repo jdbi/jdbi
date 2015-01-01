f3281cafddf60e38536357e6ddcbabd9045be965
hawkan <hakan.jonson@citerus.se>
2015-01-01 21:20:36 +0100

    Restored unnecessary white-space changes.

--- src/test/java/org/skife/jdbi/v2/DBITestCase.java
+++ src/test/java/org/skife/jdbi/v2/DBITestCase.java
@@ -95,20 +95,19 @@ public abstract class DBITestCase
     protected BasicHandle openHandle(Connection connection) throws SQLException
     {
         BasicHandle h = new BasicHandle(getTransactionHandler(),
-                getStatementLocator(),
-                new CachingStatementBuilder(new DefaultStatementBuilder()),
-                new ColonPrefixNamedParamStatementRewriter(),
-                connection,
-                new HashMap<String, Object>(),
-                new NoOpLog(),
-                TimingCollector.NOP_TIMING_COLLECTOR,
-                new MappingRegistry(),
-                new Foreman(),
-                new ContainerFactoryRegistry());
+                                        getStatementLocator(),
+                                        new CachingStatementBuilder(new DefaultStatementBuilder()),
+                                        new ColonPrefixNamedParamStatementRewriter(),
+                                        connection,
+                                        new HashMap<String, Object>(),
+                                        new NoOpLog(),
+                                        TimingCollector.NOP_TIMING_COLLECTOR,
+                                        new MappingRegistry(),
+                                        new Foreman(),
+                                        new ContainerFactoryRegistry());
         HANDLES.add(h);
         return h;
     }
-
 
     protected TransactionHandler getTransactionHandler()
     {
