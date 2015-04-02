jdbi/jdbi

commit 2b4a7247aa2b773d7914809def94520bfa0655a0

fix iterator in on-demand sqlobject: connection leak when mapper crashes

src/main/java/org/skife/jdbi/v2/sqlobject/ResultReturnThing.java
@@ -18,6 +18,7 @@ package org.skife.jdbi.v2.sqlobject;
 import com.fasterxml.classmate.ResolvedType;
 import com.fasterxml.classmate.TypeBindings;
 import com.fasterxml.classmate.members.ResolvedMethod;
+
 import org.skife.jdbi.v2.Query;
 import org.skife.jdbi.v2.ResultBearing;
 import org.skife.jdbi.v2.ResultIterator;
@@ -166,6 +167,7 @@ abstract class ResultReturnThing
             return new ResultIterator()
             {
                 private boolean closed = false;
+                private boolean hasNext = itty.hasNext();
 
                 public void close()
                 {
@@ -173,7 +175,8 @@ abstract class ResultReturnThing
                         closed = true;
                         try {
                             itty.close();
-                        } finally {
+                        }
+                        finally {
                             baton.release("iterator");
                         }
                     }
@@ -181,21 +184,31 @@ abstract class ResultReturnThing
 
                 public boolean hasNext()
                 {
-                    boolean has_next = itty.hasNext();
-                    if (!has_next) {
-                        close();
-                    }
-                    return itty.hasNext();
+                    return hasNext;
                 }
 
                 public Object next()
                 {
-                    Object rs = itty.next();
-                    boolean has_next = itty.hasNext();
-                    if (!has_next) {
+                    Object rs;
+                    try {
+                        rs = itty.next();
+                        hasNext = itty.hasNext();
+                    }
+                    catch (RuntimeException e) {
+                        closeIgnoreException();
+                        throw e;
+                    }
+                    if (!hasNext) {
                         close();
                     }
                     return rs;
+                }
+
+                @SuppressWarnings("PMD.EmptyCatchBlock")
+                public void closeIgnoreException() {
+                    try {
+                        close();
+                    } catch (RuntimeException ex) {}
                 }
 
                 public void remove()

src/test/java/org/skife/jdbi/v2/sqlobject/TestOnDemandSqlObject.java
@@ -28,12 +28,15 @@ import org.skife.jdbi.v2.ResultIterator;
 import org.skife.jdbi.v2.Something;
 import org.skife.jdbi.v2.exceptions.TransactionException;
 import org.skife.jdbi.v2.exceptions.UnableToCloseResourceException;
+import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.exceptions.DBIException;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 import org.skife.jdbi.v2.sqlobject.mixins.GetHandle;
 import org.skife.jdbi.v2.sqlobject.mixins.Transactional;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
 import org.skife.jdbi.v2.util.StringMapper;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -165,6 +168,23 @@ public class TestOnDemandSqlObject
     }
 
     @Test
+    public void testIteratorClosedOnReadError() throws Exception {
+        HandleTrackerDBI dbi = new HandleTrackerDBI(ds);
+
+        Spiffy spiffy = SqlObjectBuilder.onDemand(dbi, Spiffy.class);
+        spiffy.insert(1, "Tom");
+
+        Iterator<Something> i = spiffy.crashOnFirstRead();
+        try {
+            i.next();
+            fail();
+        } catch (DBIException ex) {
+        }
+
+        assertFalse(dbi.hasOpenedHandle());
+    }
+
+    @Test
     public void testIteratorPrepatureClose() throws Exception {
         HandleTrackerDBI dbi = new HandleTrackerDBI(ds);
 
@@ -205,6 +225,11 @@ public class TestOnDemandSqlObject
         @SqlQuery("select * from crash now")
         @Mapper(SomethingMapper.class)
         Iterator<Something> crashNow();
+
+        @SqlQuery("select name, id from something")
+        @Mapper(CrashingMapper.class)
+        Iterator<Something> crashOnFirstRead();
+
     }
 
     public static interface TransactionStuff extends GetHandle, Transactional<TransactionStuff>
@@ -225,6 +250,14 @@ public class TestOnDemandSqlObject
         @SqlQuery("all-something")
         @Mapper(SomethingMapper.class)
         Iterator<Something> findAll();
+    }
+
+    static class CrashingMapper implements ResultSetMapper<Something>
+    {
+        public Something map(int index, ResultSet r, StatementContext ctx) throws SQLException
+        {
+            throw new SQLException("protocol error");
+        }
     }
 
     public static class HandleTrackerDBI extends DBI 

commit a6d11f6d9c3df52e4ad5c605d63eb4da95a9afc1

sqlobject: fix connection leak in IteratorResultReturnThing
When the underlying ResultIterator was empty, the HandleDing
wasn't released.

This fixes a regression introduced by 2b4a7247aa2b773d7914809def94520bfa0655a0.

Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

src/main/java/org/skife/jdbi/v2/sqlobject/ResultReturnThing.java
@@ -162,12 +162,18 @@ abstract class ResultReturnThing
         protected Object result(ResultBearing q, final HandleDing baton)
         {
             final ResultIterator itty = q.iterator();
-            baton.retain("iterator");
+
+            final boolean isEmpty = !itty.hasNext();
+            if (isEmpty) {
+                itty.close();
+            } else {
+                baton.retain("iterator");
+            }
 
             return new ResultIterator()
             {
-                private boolean closed = false;
-                private boolean hasNext = itty.hasNext();
+                private boolean closed = isEmpty;
+                private boolean hasNext = !isEmpty;
 
                 @Override
                 public void close()

src/test/java/org/skife/jdbi/v2/sqlobject/TestOnDemandSqlObject.java
@@ -185,6 +185,17 @@ public class TestOnDemandSqlObject
     }
 
     @Test
+    public void testIteratorClosedIfEmpty() throws Exception {
+        HandleTrackerDBI dbi = new HandleTrackerDBI(ds);
+
+        Spiffy spiffy = SqlObjectBuilder.onDemand(dbi, Spiffy.class);
+
+        ResultIterator<Something> nothing = spiffy.findAll();
+
+        assertFalse(dbi.hasOpenedHandle());
+    }
+
+    @Test
     public void testIteratorPrepatureClose() throws Exception {
         HandleTrackerDBI dbi = new HandleTrackerDBI(ds);
 