316514015afadab7d21e0a91949b5e7f2cae81b4
Matthew Hall <matthew.hall@zanebenefits.com>
2015-09-21 19:16:26 -0600

    Have BeanMapperFactory consult column mapper registry instead of BuiltInArgumentFactory when deciding whether BeanMapperFactory can map a type.

--- src/main/java/org/skife/jdbi/v2/tweak/BeanMapperFactory.java
+++ src/main/java/org/skife/jdbi/v2/tweak/BeanMapperFactory.java
@@ -13,9 +13,7 @@
  */
 package org.skife.jdbi.v2.tweak;
 
-
 import org.skife.jdbi.v2.BeanMapper;
-import org.skife.jdbi.v2.BuiltInArgumentFactory;
 import org.skife.jdbi.v2.ResultSetMapperFactory;
 import org.skife.jdbi.v2.StatementContext;
 
@@ -24,11 +22,7 @@ public class BeanMapperFactory implements ResultSetMapperFactory
     @Override
     public boolean accepts(Class type, StatementContext ctx)
     {
-        if (BuiltInArgumentFactory.canAccept(type)) {
-            // don't interfere with built-ins
-            return false;
-        }
-        return true;
+        return ctx.columnMapperFor(type) == null;
     }
 
     @Override
