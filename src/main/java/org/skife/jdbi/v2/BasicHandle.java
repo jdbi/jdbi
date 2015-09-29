99d2a79978d038edf65a16576d6e12675367bffa
Steven Schlansker <sschlansker@opentable.com>
2015-09-29 15:44:01 -0700

    BasicHandle.close() can fail to close connection if StatementBuilder.close() fails Fixes #180

--- src/main/java/org/skife/jdbi/v2/BasicHandle.java
+++ src/main/java/org/skife/jdbi/v2/BasicHandle.java
@@ -112,16 +112,19 @@ class BasicHandle implements Handle
     public void close()
     {
         if (!closed) {
-            statementBuilder.close(getConnection());
             try {
-                connection.close();
-            }
-            catch (SQLException e) {
-                throw new UnableToCloseResourceException("Unable to close Connection", e);
-            }
-            finally {
-                log.logReleaseHandle(this);
-                closed = true;
+                statementBuilder.close(getConnection());
+            } finally {
+                try {
+                    connection.close();
+                }
+                catch (SQLException e) {
+                    throw new UnableToCloseResourceException("Unable to close Connection", e);
+                }
+                finally {
+                    log.logReleaseHandle(this);
+                    closed = true;
+                }
             }
         }
     }
