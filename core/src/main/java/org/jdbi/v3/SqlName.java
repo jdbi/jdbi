1eedfea104e8a36f86c4ae790799ae8364755583
Brian McCallister <brianm@skife.org>
2015-10-19 21:05:08 -0700

    Fix StringTemplate3 Locator for new SqlName
    Break/fix

--- core/src/main/java/org/jdbi/v3/SqlName.java
+++ core/src/main/java/org/jdbi/v3/SqlName.java
@@ -1,12 +1,20 @@
 package org.jdbi.v3;
 
 public class SqlName {
-    private final Object name;
+    private final Object   name;
     private final Object[] args;
+    private final String   string;
 
     SqlName(Object name, Object... args) {
         this.name = name;
         this.args = args;
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        for (Object arg : args) {
+            sb.append(" ");
+            sb.append(arg);
+        }
+        this.string = sb.toString();
     }
 
     public Object getName() {
@@ -19,12 +27,6 @@ public class SqlName {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(name);
-        for (Object arg : args) {
-            sb.append(" ");
-            sb.append(arg);
-        }
-        return sb.toString();
+        return string;
     }
 }
