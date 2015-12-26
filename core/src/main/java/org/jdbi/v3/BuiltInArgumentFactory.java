97b376fdda2edaa516d2bbfc2ab62d833b526c30
Steven Schlansker <sschlansker@opentable.com>
2015-12-25 18:11:09 -0800

    Simplify BuiltInArgumentFactory using lambdas, removing a ton of public API surface in the process.

--- core/src/main/java/org/jdbi/v3/BuiltInArgumentFactory.java
+++ core/src/main/java/org/jdbi/v3/BuiltInArgumentFactory.java
@@ -13,107 +13,160 @@
  */
 package org.jdbi.v3;
 
-import java.lang.reflect.Constructor;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.sql.Blob;
 import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.Map;
 
 import org.jdbi.v3.tweak.Argument;
 import org.jdbi.v3.tweak.ArgumentFactory;
 
+/**
+ * The BuiltInArgumentFactory provides instances of {@link Argument} for
+ * many core Java types.  Generally you should not need to use this
+ * class directly, but instead should bind your object with the
+ * {@link SQLStatement} convenience methods.
+ */
 public class BuiltInArgumentFactory<T> implements ArgumentFactory<T> {
+    // Care for the initialization order here, there's a fair number of statics.  Create the builders before the factory instance.
+
+    private static final ArgBuilder<String> STR_BUILDER = v -> new BuiltInArgument<>(String.class, Types.VARCHAR, PreparedStatement::setString, v);
+    private static final ArgBuilder<Object> OBJ_BUILDER = v -> new BuiltInArgument<>(Object.class, Types.NULL, PreparedStatement::setObject, v);
+    private static final Map<Class<?>, ArgBuilder<?>> BUILDERS = createInternalBuilders();
 
     public static final ArgumentFactory<?> INSTANCE = new BuiltInArgumentFactory<Object>();
 
-    private static final Map<Class<?>, P> b = new IdentityHashMap<>();
+    /**
+     * Create an Argument for a built in type.  If the type is not recognized,
+     * the result will delegate to {@link PreparedStatement#setObject(int, Object)}.
+     */
+    @SuppressWarnings("unchecked")
+    public static Argument build(Object arg) {
+        return ((ArgumentFactory<Object>)INSTANCE).build(Object.class, arg, null);
+    }
 
-    static {
-        b.put(BigDecimal.class, new P(BigDecimalArgument.class));
-        b.put(Blob.class, new P(BlobArgument.class));
-        b.put(Boolean.class, new P(BooleanArgument.class));
-        b.put(boolean.class, new P(BooleanArgument.class));
-        b.put(Byte.class, new P(ByteArgument.class));
-        b.put(byte.class, new P(ByteArgument.class));
-        b.put(byte[].class, new P(ByteArrayArgument.class));
-        b.put(Character.class, new P(CharacterArgument.class));
-        b.put(char.class, new P(CharacterArgument.class));
-        b.put(Clob.class, new P(ClobArgument.class));
-        b.put(Double.class, new P(DoubleArgument.class));
-        b.put(double.class, new P(DoubleArgument.class));
-        b.put(Float.class, new P(FloatArgument.class));
-        b.put(float.class, new P(FloatArgument.class));
-        b.put(Integer.class, new P(IntegerArgument.class));
-        b.put(int.class, new P(IntegerArgument.class));
-        b.put(java.util.Date.class, new P(JavaDateArgument.class));
-        b.put(Long.class, new P(LongArgument.class));
-        b.put(long.class, new P(LongArgument.class));
-        b.put(Object.class, new P(ObjectArgument.class));
-        b.put(Short.class, new P(ShortArgument.class));
-        b.put(short.class, new P(ShortArgument.class));
-        b.put(java.sql.Date.class, new P(SqlDateArgument.class));
-        b.put(String.class, new P(StringArgument.class));
-        b.put(Time.class, new P(TimeArgument.class));
-        b.put(Timestamp.class, new P(TimestampArgument.class));
-        b.put(URL.class, new P(URLArgument.class));
+    private static <T> void register(Map<Class<?>, ArgBuilder<?>> map, Class<T> klass, int type, StatementBinder<T> binder) {
+        register(map, klass, v -> new BuiltInArgument<T>(klass, type, binder, v));
+    }
+
+    private static <T> void register(Map<Class<?>, ArgBuilder<?>> map, Class<T> klass, ArgBuilder<T> builder) {
+        map.put(klass, builder);
+    }
+
+    /** Create a binder which calls String.valueOf on its argument and then delegates to another binder. */
+    private static <T> StatementBinder<T> stringifyValue(StatementBinder<String> real) {
+        return (p, i, v) -> real.bind(p, i, String.valueOf(v));
+    }
+
+    private static Map<Class<?>, ArgBuilder<?>> createInternalBuilders() {
+        final Map<Class<?>, ArgBuilder<?>> map = new IdentityHashMap<>();
+        register(map, BigDecimal.class, Types.NUMERIC, PreparedStatement::setBigDecimal);
+        register(map, Blob.class, Types.BLOB, PreparedStatement::setBlob);
+        register(map, Boolean.class, Types.BOOLEAN, PreparedStatement::setBoolean);
+        register(map, boolean.class, Types.BOOLEAN, PreparedStatement::setBoolean);
+        register(map, Byte.class, Types.TINYINT, PreparedStatement::setByte);
+        register(map, byte.class, Types.TINYINT, PreparedStatement::setByte);
+        register(map, byte[].class, Types.VARBINARY, PreparedStatement::setBytes);
+        register(map, Character.class, Types.CHAR, stringifyValue(PreparedStatement::setString));
+        register(map, char.class, Types.CHAR, stringifyValue(PreparedStatement::setString));
+        register(map, Clob.class, Types.CLOB, PreparedStatement::setClob);
+        register(map, Double.class, Types.DOUBLE, PreparedStatement::setDouble);
+        register(map, double.class, Types.DOUBLE, PreparedStatement::setDouble);
+        register(map, Float.class, Types.FLOAT, PreparedStatement::setFloat);
+        register(map, float.class, Types.FLOAT, PreparedStatement::setFloat);
+        register(map, Integer.class, Types.INTEGER, PreparedStatement::setInt);
+        register(map, int.class, Types.INTEGER, PreparedStatement::setInt);
+        register(map, java.util.Date.class, Types.TIMESTAMP, (p, i, v) -> p.setTimestamp(i, new Timestamp(v.getTime())));
+        register(map, Long.class, Types.INTEGER, PreparedStatement::setLong);
+        register(map, long.class, Types.INTEGER, PreparedStatement::setLong);
+        register(map, Object.class, OBJ_BUILDER);
+        register(map, Short.class, Types.SMALLINT, PreparedStatement::setShort);
+        register(map, short.class, Types.SMALLINT, PreparedStatement::setShort);
+        register(map, java.sql.Date.class, Types.DATE, PreparedStatement::setDate);
+        register(map, String.class, STR_BUILDER);
+        register(map, Time.class, Types.TIME, PreparedStatement::setTime);
+        register(map, Timestamp.class, Types.TIMESTAMP, PreparedStatement::setTimestamp);
+        register(map, URL.class, Types.DATALINK, PreparedStatement::setURL);
+        return Collections.unmodifiableMap(map);
     }
 
     @Override
     public boolean accepts(Class<?> expectedType, Object value, StatementContext ctx)
     {
-        return b.containsKey(expectedType) || value == null || value.getClass().isEnum();
+        return BUILDERS.containsKey(expectedType) || value == null || value.getClass().isEnum();
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public Argument build(Class<?> expectedType, T value, StatementContext ctx)
     {
-        P p = b.get(expectedType);
+        Class<?> buildType = value != null && expectedType == Object.class ? value.getClass() : expectedType;
+        ArgBuilder<T> v = (ArgBuilder<T>) BUILDERS.get(buildType);
 
-        if (value != null && expectedType == Object.class) {
-            P v = b.get(value.getClass());
-            if (v != null) {
-                return v.build(value);
-            }
-
-            if (value.getClass().isEnum()) {
-                return new StringArgument(((Enum<?>)value).name());
-            }
-        }
-
-        if (p != null) {
-            return p.build(value);
+        if (v != null) {
+            return v.build(value);
         }
 
         // Enums must be bound as VARCHAR.
-        if (expectedType.isEnum()) {
-            return new StringArgument(value.toString());
+        if (value != null && value.getClass().isEnum()) {
+            return STR_BUILDER.build((((Enum<?>)value).name()));
         }
 
         // Fallback to generic ObjectArgument
-        return new ObjectArgument(value);
+        return OBJ_BUILDER.build(value);
     }
 
-    private static class P
-    {
-        private final Constructor<?> ctor;
+    @FunctionalInterface
+    interface StatementBinder<T> {
+        void bind(PreparedStatement p, int index, T value) throws SQLException;
+    }
 
-        P(Class<? extends Argument> argType)
-        {
-            this.ctor = argType.getDeclaredConstructors()[0];
+    @FunctionalInterface
+    interface ArgBuilder<T> {
+        Argument build(final T value);
+    }
+
+    static final class BuiltInArgument<T> implements Argument {
+        private final T value;
+        private final boolean isArray;
+        private final int type;
+        private final StatementBinder<T> binder;
+
+        private BuiltInArgument(Class<T> klass, int type, StatementBinder<T> binder, T value) {
+            this.binder = binder;
+            this.isArray = klass.isArray();
+            this.type = type;
+            this.value = value;
         }
 
-        public Argument build(Object value)
-        {
-            try {
-                return (Argument) ctor.newInstance(value);
+        @Override
+        public void apply(int position, PreparedStatement statement, StatementContext ctx) throws SQLException {
+            if (value == null) {
+                statement.setNull(position, type);
+                return;
             }
-            catch (Exception e) {
-                throw new IllegalStateException(e);
+            binder.bind(statement, position, value);
+        }
+
+        @Override
+        public String toString() {
+            if (isArray) {
+                return Arrays.toString((Object[]) value);
             }
+            return String.valueOf(value);
+        }
+
+        StatementBinder<T> getStatementBinder() {
+            return binder;
         }
     }
 }
