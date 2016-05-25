7d65abb45c2c083e9f1122c8e2b6c116ea8114fd
Matthew Hall <matthew.hall@peoplekeep.com>
2016-05-24 21:59:12 -0600

    Fix up javadoc in StatementBuilderFactory

--- core/src/main/java/org/jdbi/v3/tweak/StatementBuilderFactory.java
+++ core/src/main/java/org/jdbi/v3/tweak/StatementBuilderFactory.java
@@ -23,7 +23,7 @@ import java.sql.Connection;
 public interface StatementBuilderFactory
 {
     /**
-     * Obtain a StatementBuilder, called when a new handle is opened
+     * @return a StatementBuilder, called when a new handle is opened
      */
     StatementBuilder createStatementBuilder(Connection conn);
 }
