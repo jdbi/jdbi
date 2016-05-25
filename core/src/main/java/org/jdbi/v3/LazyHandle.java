e8d0d2ec384a69bb6562089fbb9e7774a631790e
Matthew Hall <matthew.hall@peoplekeep.com>
2016-05-24 21:22:53 -0600

    Fix up javadoc, and some checkstyle warnings

--- core/src/main/java/org/jdbi/v3/LazyHandle.java
+++ core/src/main/java/org/jdbi/v3/LazyHandle.java
@@ -21,7 +21,7 @@ class LazyHandle implements Supplier<Handle>, AutoCloseable {
     private volatile Handle handle;
     private volatile boolean closed = false;
 
-    public LazyHandle(DBI dbi) {
+    LazyHandle(DBI dbi) {
         this.dbi = dbi;
     }
 
