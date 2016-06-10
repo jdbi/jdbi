e94a0416f5b85dcea76f9ce3a6eb85c33874fe95
Steven Schlansker <sschlansker@opentable.com>
2016-06-09 19:17:58 -0700

    GenericTypeTest to v3.util

--- core/src/test/java/org/jdbi/v3/util/GenericTypeTest.java
+++ core/src/test/java/org/jdbi/v3/util/GenericTypeTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jdbi.v3;
+package org.jdbi.v3.util;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
