da2c182e9830604551a660094d9df6eb081ec67e
Steven Schlansker <sschlansker@opentable.com>
2016-08-01 13:03:30 -0700

    jdbi3: Add batching documentation

---
+++ docs/src/test/java/jdbi/doc/BatchTest.java
@@ -0,0 +1,107 @@
+package jdbi.doc;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.jdbi.v3.core.Batch;
+import org.jdbi.v3.core.H2DatabaseRule;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.PreparedBatch;
+import org.jdbi.v3.sqlobject.BindBean;
+import org.jdbi.v3.sqlobject.SqlBatch;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jdbi.v3.sqlobject.SqlQuery;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BatchTest {
+
+    @Rule
+    public H2DatabaseRule db = new H2DatabaseRule().withPlugin(new SqlObjectPlugin());
+    private Handle handle;
+
+    @Before
+    public void getHandle() {
+        handle = db.getSharedHandle();
+        handle.execute("CREATE TABLE fruit (id INT PRIMARY KEY, name VARCHAR)");
+    }
+
+    @Test
+    // tag::simpleBatch[]
+    public void testSimpleBatch() {
+        Batch batch = handle.createBatch();
+
+        batch.add("INSERT INTO fruit VALUES(0, 'apple')");
+        batch.add("INSERT INTO fruit VALUES(1, 'banana')");
+
+        int[] rowsModified = batch.execute();
+        assertArrayEquals(new int[] {1, 1}, rowsModified);
+        assertEquals(2,
+                handle.createQuery("SELECT count(1) FROM fruit")
+                .mapTo(int.class)
+                .findOnly()
+                .intValue());
+    }
+    // end::simpleBatch[]
+
+    @Test
+    // tag::preparedBatch[]
+    public void testPreparedBatch() {
+        PreparedBatch batch = handle.prepareBatch("INSERT INTO fruit VALUES(:id, :name)");
+
+        batch.bind("id", 0);
+        batch.bind("name", "apple");
+        batch.add();
+
+        batch.bind("id", 1);
+        batch.bind("name", "banana");
+        batch.add();
+
+        int[] rowsModified = batch.execute();
+        assertArrayEquals(new int[] {1, 1}, rowsModified);
+        assertEquals(2,
+                handle.createQuery("SELECT count(1) FROM fruit")
+                .mapTo(int.class)
+                .findOnly()
+                .intValue());
+    }
+    // end::preparedBatch[]
+
+    @Test
+    // tag::sqlObjectBatch[]
+    public void testSqlObjectBatch() {
+        BasketOfFruit basket = handle.attach(BasketOfFruit.class);
+
+        int[] rowsModified = basket.fillBasket(Arrays.asList(
+                new Fruit(0, "apple"),
+                new Fruit(1, "banana")));
+
+        assertArrayEquals(new int[] {1, 1}, rowsModified);
+        assertEquals(2, basket.countFruit());
+    }
+
+    public interface BasketOfFruit {
+        @SqlBatch("INSERT INTO fruit VALUES(:id, :name)")
+        int[] fillBasket(@BindBean Collection<Fruit> fruits);
+
+        @SqlQuery("SELECT count(1) FROM fruit")
+        int countFruit();
+    }
+
+    public class Fruit {
+        private final int id;
+        private final String name;
+
+        Fruit(int id, String name) {
+            this.id = id; this.name = name;
+        }
+
+        public int getId() { return id; }
+        public String getName() { return name; }
+    }
+    // end::sqlObjectBatch[]
+}
