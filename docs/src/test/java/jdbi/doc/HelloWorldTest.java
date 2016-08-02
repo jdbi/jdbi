7bc9c8940de1a574a9d80b48274c4e668c11b451
Steven Schlansker <sschlansker@opentable.com>
2016-08-02 12:44:52 -0700

    docs: Flesh out front page a bit more

--- docs/src/test/java/jdbi/doc/HelloWorldTest.java
+++ docs/src/test/java/jdbi/doc/HelloWorldTest.java
@@ -13,15 +13,51 @@
  */
 package jdbi.doc;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.ConstructorMapper;
 import org.junit.Test;
 
 public class HelloWorldTest {
 
     @Test
-    // tag::frontPage[]
     public void helloJdbi() {
-        Jdbi dbi;
+        // tag::frontPage[]
+        // H2 in-memory database
+        Jdbi dbi = Jdbi.create("jdbc:h2:mem:test");
+        // Easy scope-based transactions
+        List<User> users = dbi.inTransaction((handle, status) -> {
+            handle.execute("CREATE TABLE user (id INTEGER PRIMARY KEY, name VARCHAR)");
+            handle.createStatement("INSERT INTO user(id, name) VALUES (:id, :name)")
+                .bind("id", 0)   // Bind arguments by 0-indexed position
+                .bind(1, "You!") // Or by name
+                .execute();
+
+            handle.createStatement("INSERT INTO user(id, name) VALUES (:id, :name)")
+                .bindFromProperties(new User(1, "Me")) // You can also bind custom types
+                .execute();
+
+            // Easy mapping to your types
+            handle.registerRowMapper(ConstructorMapper.factoryFor(User.class));
+            return handle.createQuery("SELECT * FROM user ORDER BY id")
+                .mapTo(User.class)
+                .list();
+        });
+        // end::frontPage[]
+        assertEquals(0, users.get(0).getId());
+        assertEquals(1, users.get(1).getId());
+        assertEquals("You!", users.get(0).getName());
+        assertEquals("Me",   users.get(1).getName());
     }
-    // end::frontPage[]
+
+    public static class User {
+        private final int id;
+        private final String name;
+        public User(int id, String name) { this.id = id; this.name = name; }
+        public int getId() { return id; }
+        public String getName() { return name; }
+    }
 }
