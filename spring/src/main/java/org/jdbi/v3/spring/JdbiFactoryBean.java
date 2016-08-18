fd5b5dffa92ad0bfba31e017ffac3a3d0713cf8d
Steven Schlansker <sschlansker@opentable.com>
2016-08-18 13:51:25 -0700

    spring: DBI -> Jdbi

--- spring/src/main/java/org/jdbi/v3/spring/JdbiFactoryBean.java
+++ spring/src/main/java/org/jdbi/v3/spring/JdbiFactoryBean.java
@@ -31,17 +31,17 @@ import org.springframework.jdbc.datasource.DataSourceUtils;
  * Utility class which constructs an {@link Jdbi} instance which can conveniently participate
  * in Spring's transaction management system.
  */
-public class DBIFactoryBean implements FactoryBean<Jdbi>
+public class JdbiFactoryBean implements FactoryBean<Jdbi>
 {
     private DataSource dataSource;
     private final Map<String, Object> globalDefines = new HashMap<>();
 
     private Collection<JdbiPlugin> plugins = Collections.emptyList();
 
-    public DBIFactoryBean() {
+    public JdbiFactoryBean() {
     }
 
-    public DBIFactoryBean(DataSource dataSource) {
+    public JdbiFactoryBean(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -94,7 +94,7 @@ public class DBIFactoryBean implements FactoryBean<Jdbi>
     }
 
     @Autowired(required=false)
-    public DBIFactoryBean setPlugins(Collection<JdbiPlugin> plugins)
+    public JdbiFactoryBean setPlugins(Collection<JdbiPlugin> plugins)
     {
         this.plugins = plugins;
         return this;
