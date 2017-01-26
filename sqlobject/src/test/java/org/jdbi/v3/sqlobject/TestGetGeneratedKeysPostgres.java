0d148a7ebad237226d1bc7c5135f4c8a1730000d
Matthew Hall <qualidafial@gmail.com>
2017-01-25 20:22:18 -0700

    Move test DAOs to bottom of class; move distinct setup to each DAO since there's no shared setup.

--- sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestGetGeneratedKeysPostgres.java
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestGetGeneratedKeysPostgres.java
@@ -13,7 +13,6 @@
  */
 package org.jdbi.v3.sqlobject;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.ResultSet;
@@ -33,8 +32,6 @@ import org.jdbi.v3.sqlobject.statement.SqlBatch;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jdbi.v3.sqlobject.statement.UseRowMapper;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -43,23 +40,88 @@ public class TestGetGeneratedKeysPostgres
     @Rule
     public PgDatabaseRule dbRule = new PgDatabaseRule().withPlugin(new SqlObjectPlugin());
 
-    @Before
-    public void setUp() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("create sequence id_sequence INCREMENT 1 START WITH 100");
-            handle.execute("create table if not exists something (name text, id int DEFAULT nextval('id_sequence'), CONSTRAINT something_id PRIMARY KEY ( id ));");
+    @Test
+    public void testFoo() throws Exception {
+        dbRule.getJdbi().useExtension(DAO.class, dao -> {
+            dao.createSequence();
+            dao.createTable();
+
+            long brian_id = dao.insert("Brian");
+            long keith_id = dao.insert("Keith");
+
+            assertThat(dao.findNameById(brian_id)).isEqualTo("Brian");
+            assertThat(dao.findNameById(keith_id)).isEqualTo("Keith");
         });
     }
 
-    @After
-    public void tearDown() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("drop table something");
-            handle.execute("drop sequence id_sequence");
+    @Test
+    public void testBatch() throws Exception {
+        dbRule.getJdbi().useExtension(DAO.class, dao -> {
+            dao.createSequence();
+            dao.createTable();
+
+            int[] ids = dao.insert(Arrays.asList("Burt", "Macklin"));
+
+            assertThat(dao.findNameById(ids[0])).isEqualTo("Burt");
+            assertThat(dao.findNameById(ids[1])).isEqualTo("Macklin");
+        });
+    }
+
+    @Test
+    public void testUseRowMapperUpdate() throws Exception {
+        dbRule.getJdbi().useExtension(UseRowMapperDao.class, dao -> {
+            dao.createTable();
+
+            IdCreateTime result = dao.insert("foo");
+
+            assertThat(result.id).isEqualTo(1);
+            assertThat(result.createdOn).isNotNull();
+        });
+    }
+
+    @Test
+    public void testUseRowMapperBatch() throws Exception {
+        dbRule.getJdbi().useExtension(UseRowMapperDao.class, dao -> {
+            dao.createTable();
+
+            List<IdCreateTime> results = dao.insertBatch("foo", "bar");
+
+            assertThat(results).extracting(ic -> ic.id).containsExactly(1L, 2L);
+            assertThat(results).extracting(ic -> ic.createdOn).hasSize(2).doesNotContainNull();
+        });
+    }
+
+    @Test
+    public void testRegisterRowMapperUpdate() throws Exception {
+        dbRule.getJdbi().useExtension(RegisterRowMapperDao.class, dao -> {
+            dao.createTable();
+
+            IdCreateTime result = dao.insert("foo");
+
+            assertThat(result.id).isEqualTo(1);
+            assertThat(result.createdOn).isNotNull();
+        });
+    }
+
+    @Test
+    public void testRegisterRowMapperBatch() throws Exception {
+        dbRule.getJdbi().useExtension(RegisterRowMapperDao.class, dao -> {
+            dao.createTable();
+
+            List<IdCreateTime> results = dao.insertBatch("foo", "bar");
+
+            assertThat(results).extracting(ic -> ic.id).containsExactly(1L, 2L);
+            assertThat(results).extracting(ic -> ic.createdOn).hasSize(2).doesNotContainNull();
         });
     }
 
     public interface DAO {
+        @SqlUpdate("create sequence id_sequence INCREMENT 1 START WITH 100")
+        void createSequence();
+
+        @SqlUpdate("create table if not exists something (name text, id int DEFAULT nextval('id_sequence'), CONSTRAINT something_id PRIMARY KEY ( id ))")
+        void createTable();
+
         @SqlUpdate("insert into something (name, id) values (:name, nextval('id_sequence'))")
         @GetGeneratedKeys("id")
         long insert(@Bind("name") String name);
@@ -72,55 +134,10 @@ public class TestGetGeneratedKeysPostgres
         String findNameById(@Bind long id);
     }
 
-    @Test
-    public void testFoo() throws Exception {
-        dbRule.getJdbi().useExtension(DAO.class, dao -> {
-            long brian_id = dao.insert("Brian");
-            long keith_id = dao.insert("Keith");
-
-            assertThat(dao.findNameById(brian_id)).isEqualTo("Brian");
-            assertThat(dao.findNameById(keith_id)).isEqualTo("Keith");
-        });
-    }
-
-    @Test
-    public void testBatch() throws Exception {
-        dbRule.getJdbi().useExtension(DAO.class, dao -> {
-            int[] ids = dao.insert(Arrays.asList("Burt", "Macklin"));
-            assertThat(dao.findNameById(ids[0])).isEqualTo("Burt");
-            assertThat(dao.findNameById(ids[1])).isEqualTo("Macklin");
-        });
-    }
-
-    @Test
-    public void testUseRowMapperUpdate() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("drop table something");
-            handle.execute("create table something (id serial, name varchar(50), created_on timestamp default now())");
-
-            UseRowMapperDao dao = handle.attach(UseRowMapperDao.class);
-
-            IdCreateTime result = dao.insert("foo");
-            assertThat(result.id).isEqualTo(1);
-            assertThat(result.createdOn).isNotNull();
-        });
-    }
-
-    @Test
-    public void testUseRowMapperBatch() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("drop table something");
-            handle.execute("create table something (id serial, name varchar(50), created_on timestamp default now())");
-
-            UseRowMapperDao dao = handle.attach(UseRowMapperDao.class);
-
-            List<IdCreateTime> results = dao.insertBatch("foo", "bar");
-            assertThat(results).extracting(ic -> ic.id).containsExactly(1L, 2L);
-            assertThat(results).extracting(ic -> ic.createdOn).hasSize(2).doesNotContainNull();
-        });
-    }
-
     public interface UseRowMapperDao {
+        @SqlUpdate("create table something (id serial, name varchar(50), created_on timestamp default now())")
+        void createTable();
+
         @SqlUpdate("insert into something(name) values (:name)")
         @GetGeneratedKeys({"id", "created_on"})
         @UseRowMapper(IdCreateTimeMapper.class)
@@ -132,35 +149,10 @@ public class TestGetGeneratedKeysPostgres
         List<IdCreateTime> insertBatch(String... name);
     }
 
-    @Test
-    public void testRegisterRowMapperUpdate() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("drop table something");
-            handle.execute("create table something (id serial, name varchar(50), created_on timestamp default now())");
-
-            RegisterRowMapperDao dao = handle.attach(RegisterRowMapperDao.class);
-
-            IdCreateTime result = dao.insert("foo");
-            assertThat(result.id).isEqualTo(1);
-            assertThat(result.createdOn).isNotNull();
-        });
-    }
-
-    @Test
-    public void testRegisterRowMapperBatch() throws Exception {
-        dbRule.getJdbi().useHandle(handle -> {
-            handle.execute("drop table something");
-            handle.execute("create table something (id serial, name varchar(50), created_on timestamp default now())");
-
-            RegisterRowMapperDao dao = handle.attach(RegisterRowMapperDao.class);
-
-            List<IdCreateTime> results = dao.insertBatch("foo", "bar");
-            assertThat(results).extracting(ic -> ic.id).containsExactly(1L, 2L);
-            assertThat(results).extracting(ic -> ic.createdOn).hasSize(2).doesNotContainNull();
-        });
-    }
-
     public interface RegisterRowMapperDao {
+        @SqlUpdate("create table something (id serial, name varchar(50), created_on timestamp default now())")
+        void createTable();
+
         @SqlUpdate("insert into something(name) values (:name)")
         @GetGeneratedKeys({"id", "created_on"})
         @RegisterRowMapper(IdCreateTimeMapper.class)
