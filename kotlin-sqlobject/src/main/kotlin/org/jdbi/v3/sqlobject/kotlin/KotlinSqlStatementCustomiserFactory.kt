a8dff8c2938a13eb58a9bc42d53e040d58a6964b
Alex Harin <alex@wrisk.co>
2017-02-26 15:02:33 +0000

    Fix intellij warnings
    Moved type unwrapping logic out of recursive function

--- kotlin-sqlobject/src/main/kotlin/org/jdbi/v3/sqlobject/kotlin/KotlinSqlStatementCustomiserFactory.kt
+++ kotlin-sqlobject/src/main/kotlin/org/jdbi/v3/sqlobject/kotlin/KotlinSqlStatementCustomiserFactory.kt
@@ -13,11 +13,11 @@
  */
 package org.jdbi.v3.sqlobject.kotlin
 
-import org.jdbi.v3.core.generic.GenericTypes.findGenericParameter
-import org.jdbi.v3.core.generic.GenericTypes.getErasedType
+import org.jdbi.v3.core.generic.GenericTypes
 import org.jdbi.v3.core.kotlin.isKotlinClass
 import org.jdbi.v3.core.statement.PreparedBatch
 import org.jdbi.v3.core.statement.SqlStatement
+import org.jdbi.v3.sqlobject.SingleValue
 import org.jdbi.v3.sqlobject.customizer.SqlStatementParameterCustomizer
 import org.jdbi.v3.sqlobject.statement.ParameterCustomizerFactory
 import java.lang.reflect.Method
@@ -33,49 +33,52 @@ class KotlinSqlStatementCustomiserFactory : ParameterCustomizerFactory {
 
 
     override fun createForParameter(sqlObjectType: Class<*>, method: Method, parameter: Parameter, paramIdx: Int): SqlStatementParameterCustomizer {
-        val paramType = parameter.getParameterizedType()
 
-        val bindName = if (parameter.isNamePresent()) {
-            parameter.getName()
+        val bindName = if (parameter.isNamePresent) {
+            parameter.name
         } else {
             method.kotlinFunction?.parameters?.dropWhile { it.kind != KParameter.Kind.VALUE }?.toList()?.get(paramIdx)?.name
-        } ?: throw UnsupportedOperationException("A parameter was not given a name, "
-                + "and parameter name data is not present in the class file, for: "
-                + parameter.getDeclaringExecutable() + " :: " + parameter)
+        } ?: throw UnsupportedOperationException("A parameter was not given a name, and parameter name data is not present in the class file, for: " +
+                "${parameter.declaringExecutable} :: $parameter")
 
 
-        fun bind(q: SqlStatement<*>, bindToParm: String?, bindAsType: Type, value: Any?, prefix: String = "") {
-            val type = if (q is PreparedBatch) {
-                // FIXME BatchHandler should extract the iterable/iterator element type and pass it to the binder
-                val erasedType = getErasedType(bindAsType)
-                if (Iterable::class.java.isAssignableFrom(erasedType)) {
-                    findGenericParameter(bindAsType, Iterable::class.java).get()
-                } else if (Iterator::class.java.isAssignableFrom(erasedType)) {
-                    findGenericParameter(bindAsType, Iterator::class.java).get()
-                } else {
-                    bindAsType
-                }
-            } else {
-                bindAsType
-            }
+        fun bind(q: SqlStatement<*>, bindToParm: String, type: Type, value: Any?, prefix: String = "") {
 
-            val erasedType = getErasedType(type)
+            val erasedType = GenericTypes.getErasedType(type)
             if (erasedType.isKotlinClass()) {
                 @Suppress("UNCHECKED_CAST")
                 (erasedType.kotlin as KClass<Any>).declaredMemberProperties.forEach { subProp ->
-                    bind(q, subProp.name, subProp.returnType.javaType, if (value == null) null else subProp.get(value), "${prefix}${bindToParm}.")
+                    bind(q, subProp.name, subProp.returnType.javaType, if (value == null) null else subProp.get(value), "$prefix$bindToParm.")
                 }
             } else {
-                if (bindToParm != null) {
-                    q.bindByType("${prefix}${bindToParm}", value, type)
-                } else if (prefix.isNullOrBlank()) {
+                q.bindByType("$prefix$bindToParm", value, type)
+                if (prefix.isBlank()) {
                     // we can't really bind sub items by order
                     q.bindByType(paramIdx, value, type)
                 }
             }
         }
         return SqlStatementParameterCustomizer { stmt, arg ->
-            bind(stmt, bindName, paramType, arg)
+            val paramType = parameter.parameterizedType
+
+            val type = if (stmt is PreparedBatch && !parameter.isAnnotationPresent(SingleValue::class.java)) {
+                // FIXME BatchHandler should extract the iterable/iterator element type and pass it to the binder
+                val erasedType = GenericTypes.getErasedType(paramType)
+                if (Iterable::class.java.isAssignableFrom(erasedType)) {
+                    GenericTypes.findGenericParameter(paramType, Iterable::class.java).get()
+                } else if (Iterator::class.java.isAssignableFrom(erasedType)) {
+                    GenericTypes.findGenericParameter(paramType, Iterator::class.java).get()
+                } else if (GenericTypes.isArray(paramType)) {
+                    (paramType as Class<*>).componentType
+                } else {
+                    paramType
+                }
+            } else {
+                paramType
+            }
+
+            bind(stmt, bindName, type, arg)
+
         }
     }
 }
