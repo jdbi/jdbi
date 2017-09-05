603914dc61eae3fc907ee30c74d9ccbeaba6c0db
Steven Schlansker <sschlansker@opentable.com>
2017-09-04 19:50:18 -0700

    Simple Configuration documentation
    Fixes #719

---
+++ docs/src/test/java/jdbi/doc/ExampleConfig.java
@@ -0,0 +1,45 @@
+package jdbi.doc;
+
+import org.jdbi.v3.core.config.JdbiConfig;
+
+// tag::exampleConfig[]
+public class ExampleConfig implements JdbiConfig<ExampleConfig>{
+
+    private String color;
+    private int number;
+
+    public ExampleConfig() {
+        color = "purple";
+        number = 42;
+    }
+
+    private ExampleConfig(ExampleConfig other) {
+        this.color = other.color;
+        this.number = other.number;
+    }
+
+    public ExampleConfig setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public ExampleConfig setNumber(int number) {
+        this.number = number;
+        return this;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public ExampleConfig createCopy() {
+        return new ExampleConfig(this);
+    }
+
+}
+// end::exampleConfig[]
