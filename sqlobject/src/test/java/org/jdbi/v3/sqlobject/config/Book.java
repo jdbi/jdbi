c15a6fa4ac5ccc35304d24e4b322ea6d64a28bdd
Leon <Leon.c@gmx.de>
2018-04-13 13:01:40 +0200

    Documentation and removed unused import

--- sqlobject/src/test/java/org/jdbi/v3/sqlobject/config/Book.java
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/config/Book.java
@@ -14,7 +14,6 @@
 package org.jdbi.v3.sqlobject.config;
 
 import org.immutables.value.Value;
-import org.jdbi.v3.core.mapper.reflect.ImmutableImplementation;
 
 @Value.Immutable
 public interface Book {
