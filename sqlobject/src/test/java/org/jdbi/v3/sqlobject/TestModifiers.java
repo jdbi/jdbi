1d19ad51a6fbd19235cf3131690edf0d751896f8
Marnes <marnick.leau@gmail.com>
2018-04-14 00:10:28 +0200

    build fix, release notes

--- sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestModifiers.java
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestModifiers.java
@@ -13,27 +13,26 @@
  */
 package org.jdbi.v3.sqlobject;
 
-import static org.jdbi.v3.core.transaction.TransactionIsolationLevel.READ_UNCOMMITTED;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
-
-import org.jdbi.v3.core.rule.H2DatabaseRule;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Something;
 import org.jdbi.v3.core.mapper.SomethingMapper;
+import org.jdbi.v3.core.rule.H2DatabaseRule;
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.FetchSize;
 import org.jdbi.v3.sqlobject.customizer.MaxRows;
 import org.jdbi.v3.sqlobject.customizer.QueryTimeOut;
-import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
-import org.jdbi.v3.sqlobject.transaction.Transaction;
-import org.jdbi.v3.sqlobject.transaction.Transactional;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jdbi.v3.sqlobject.transaction.Transaction;
+import org.jdbi.v3.sqlobject.transaction.Transactional;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jdbi.v3.core.transaction.TransactionIsolationLevel.READ_UNCOMMITTED;
 
 public class TestModifiers
 {
@@ -154,7 +153,7 @@ public class TestModifiers
 
 
         @SqlQuery("select id, name from something")
-        List<Something> findAllWithMaxRows(@MaxRows(1) int fetchSize);
+        List<Something> findAllWithMaxRows(@MaxRows int fetchSize);
 
         @SqlQuery("select id, name from something")
         @MaxRows(1)
