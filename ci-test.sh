179418bb2ceae5a206bef5032df7629f460b64bf
Matthew Hall <qualidafial@gmail.com>
2018-04-17 22:45:39 -0600

    Use running JDK version instead of properties to to activate version-specific profiles.

--- ci-test.sh
+++ ci-test.sh
@@ -2,10 +2,6 @@
 
 OPTS="-Dmaven.javadoc.skip=true -Dbasepom.check.skip-basic=true -Dbasepom.check.skip-findbugs=true -Dbasepom.check.skip-pmd=true -Dbasepom.check.skip-checkstyle=true -B"
 
-if [ "$JDK_VERSION" == "9" ]; then
-  OPTS="$OPTS -Djdk9"
-fi
-
 PROFILES="toolchains"
 
 # Oracle profile disabled until they fix their orai18n JAR artifact on maven.oracle.com repository.
