2c035546477db546f8d6e2113851dc39e9a637dc
Steven Schlansker <sschlansker@opentable.com>
2018-04-22 12:46:49 -0700

    Add a test to show jsonb query '?' can be escaped as '??' and work

---
+++ postgres/src/test/java/org/jdbi/v3/postgres/TestJsonOperator.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdbi.v3.postgres;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.jdbi.v3.testing.JdbiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestJsonOperator {
+    @Rule
+    public JdbiRule db = PostgresDbRule.rule();
+
+    @Test
+    public void testJsonQuery() throws Exception {
+        assertThat(db.getHandle()
+                .createQuery("SELECT '{\"a\":1, \"b\":2}'::jsonb ?? 'b'")
+                .mapTo(boolean.class)
+                .findOnly())
+            .isEqualTo(true);
+    }
+}
