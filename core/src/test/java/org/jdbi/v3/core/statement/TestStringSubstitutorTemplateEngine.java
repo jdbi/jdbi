1181ae459e88ced635d4b47fa92a3e01629bd70e
Marnes <marnick.leau@gmail.com>
2018-04-24 21:37:41 +0200

    StringSubstitutor TemplateEngine

---
+++ core/src/test/java/org/jdbi/v3/core/statement/TestStringSubstitutorTemplateEngine.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdbi.v3.core.statement;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.rule.DatabaseRule;
+import org.jdbi.v3.core.rule.H2DatabaseRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestStringSubstitutorTemplateEngine {
+    @Rule
+    public DatabaseRule db = new H2DatabaseRule();
+
+    private Jdbi jdbi;
+
+    @Before
+    public void before() {
+        jdbi = db.getJdbi();
+
+        jdbi.getConfig(SqlStatements.class).setTemplateEngine(new StringSubstitutorTemplateEngine());
+    }
+
+    @Test
+    public void foo() {
+        String out = jdbi.withHandle(h -> h.createQuery("select * from (values(${foo}))")
+            .define("foo", "'bar'")
+            .mapTo(String.class)
+            .findOnly());
+
+        assertThat(out).isEqualTo("bar");
+    }
+}
