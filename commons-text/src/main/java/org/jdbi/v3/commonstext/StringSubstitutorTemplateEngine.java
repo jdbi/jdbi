e947bbfe4dd4bcc8bb9f224e1c8e999a086c5f95
Matthew Hall <qualidafial@gmail.com>
2018-05-09 11:59:44 -0600

    Rename org.jdbi.v3.stringsubstitor package to .commonstext

--- commons-text/src/main/java/org/jdbi/v3/commonstext/StringSubstitutorTemplateEngine.java
+++ commons-text/src/main/java/org/jdbi/v3/commonstext/StringSubstitutorTemplateEngine.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jdbi.v3.stringsubstitutor;
+package org.jdbi.v3.commonstext;
 
 import java.util.function.Consumer;
 import org.apache.commons.text.StringSubstitutor;
