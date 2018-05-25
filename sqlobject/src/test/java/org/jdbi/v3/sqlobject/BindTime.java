2c41696e4330c700e229f7166590dd8838810a42
Marnes <marnick.leau@gmail.com>
2018-05-26 00:25:15 +0200

    move annotation to own class so build succeeds (?)

---
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/BindTime.java
@@ -0,0 +1,37 @@
+package org.jdbi.v3.sqlobject;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.time.Clock;
+import java.time.OffsetDateTime;
+import org.jdbi.v3.core.config.JdbiConfig;
+import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizer;
+import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizerFactory;
+import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizingAnnotation;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SqlStatementCustomizingAnnotation(BindTime.Factory.class)
+public @interface BindTime {
+
+  class Factory implements SqlStatementCustomizerFactory {
+    @Override
+    public SqlStatementCustomizer createForType(Annotation annotation, Class<?> sqlObjectType) {
+      return stmt -> stmt.bind("now", OffsetDateTime.now(stmt.getConfig(Config.class).clock));
+    }
+  }
+
+  class Config implements JdbiConfig<Config> {
+    public Clock clock;
+
+    @Override
+    public Config createCopy() {
+      Config copy = new Config();
+      copy.clock = this.clock;
+      return copy;
+    }
+  }
+}
--- sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestInheritedAnnotations.java
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/TestInheritedAnnotations.java
@@ -13,35 +13,23 @@
  */
 package org.jdbi.v3.sqlobject;
 
-import static java.time.temporal.ChronoUnit.SECONDS;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.time.Clock;
 import java.time.Instant;
-import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.Optional;
-
 import org.jdbi.v3.core.Handle;
-import org.jdbi.v3.core.config.JdbiConfig;
 import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 import org.jdbi.v3.core.rule.H2DatabaseRule;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
-import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizer;
-import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizerFactory;
-import org.jdbi.v3.sqlobject.customizer.SqlStatementCustomizingAnnotation;
 import org.jdbi.v3.sqlobject.locator.UseClasspathSqlLocator;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestInheritedAnnotations {
   @Rule
@@ -162,28 +150,3 @@ public class TestInheritedAnnotations {
         }
     }
   }
-
-  @Target(ElementType.TYPE)
-  @Retention(RetentionPolicy.RUNTIME)
-  @SqlStatementCustomizingAnnotation(BindTime.Factory.class)
-  public @interface BindTime {
-
-    class Factory implements SqlStatementCustomizerFactory {
-      @Override
-      public SqlStatementCustomizer createForType(Annotation annotation, Class<?> sqlObjectType) {
-        return stmt -> stmt.bind("now", OffsetDateTime.now(stmt.getConfig(Config.class).clock));
-      }
-    }
-
-    class Config implements JdbiConfig<Config> {
-      public Clock clock;
-
-      @Override
-      public Config createCopy() {
-        Config copy = new Config();
-        copy.clock = this.clock;
-        return copy;
-      }
-    }
-  }
-}
