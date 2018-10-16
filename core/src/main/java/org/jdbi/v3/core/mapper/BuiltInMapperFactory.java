2da236f7a83deaa05dc5088f3f126fba6ac33bc0
Marnes <marnick.leau@gmail.com>
2018-10-16 19:28:01 +0200

    liiiceeenseee heeaaddeerrr

--- core/src/main/java/org/jdbi/v3/core/mapper/BuiltInMapperFactory.java
+++ core/src/main/java/org/jdbi/v3/core/mapper/BuiltInMapperFactory.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jdbi.v3.core.mapper;
 
 import java.lang.reflect.Type;
@@ -8,6 +21,9 @@ import org.jdbi.v3.core.array.SqlArrayMapperFactory;
 import org.jdbi.v3.core.config.ConfigRegistry;
 import org.jdbi.v3.core.internal.JdbiStreams;
 
+/**
+ * @deprecated will be replaced by an opt-out plugin to give the core no hardwired behavior
+ */
 @Deprecated
 // TODO remove this entire class, move functionality to BuiltInSupportPlugin
 public class BuiltInMapperFactory implements ColumnMapperFactory {
