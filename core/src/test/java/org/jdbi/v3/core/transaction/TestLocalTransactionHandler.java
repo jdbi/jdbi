jdbi/jdbi

commit c87c667a6d8c64795a83845b6e79cb728aa40ef6

Catch Throwable instead of Exception in rollback failure

core/src/main/java/org/jdbi/v3/core/transaction/LocalTransactionHandler.java
@@ -139,7 +139,7 @@ public class LocalTransactionHandler implements TransactionHandler {
         } catch (Throwable e) {
             try {
                 handle.rollback();
-            } catch (Exception rollback) {
+            } catch (Throwable rollback) {
                 e.addSuppressed(rollback);
             }
             throw (X) e;

commit 29796d0bbd0683742e1c881bf4248421d9f19b77

Fix ClassCastException on `Error` thrown by `LocalTransactionHandler`.
LocalTransactionHandler would attempt to cast the caught Throwable to
Exception, which would fail for non-Exception Throwables like Error.

This bug was introduced in c87c667a6d8c64795a83845b6e79cb728aa40ef6

core/src/main/java/org/jdbi/v3/core/transaction/LocalTransactionHandler.java
@@ -142,7 +142,7 @@ public class LocalTransactionHandler implements TransactionHandler {
             } catch (Throwable rollback) {
                 e.addSuppressed(rollback);
             }
-            throw (X) e;
+            throw e;
         }
 
         didTxnRollback.remove();

core/src/test/java/org/jdbi/v3/core/transaction/TestLocalTransactionHandler.java
@@ -53,4 +53,20 @@ public class TestLocalTransactionHandler {
             assertThat(e.getSuppressed()[0]).isSameAs(inner);
         }
     }
+
+    @Test
+    public void testThrowError() throws Exception {
+        Error error = new Error("Transaction throws!");
+
+        Mockito.when(c.getAutoCommit()).thenReturn(true);
+        Mockito.when(h.getConnection()).thenReturn(c);
+
+        try {
+            new LocalTransactionHandler().inTransaction(h, x -> {
+                throw error;
+            });
+        } catch (Throwable throwable) {
+            assertThat(throwable).isSameAs(error);
+        }
+    }
 }