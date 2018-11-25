906671f96bc8126f9600be95c54852fe15d8f8b8
Marnes <marnick.leau@gmail.com>
2018-11-25 21:19:34 +0100

    custom package-info

---
+++ core/src/main/java/org/jdbi/v3/lib/internal/org_jooq/jool_java_8/v0_9_14/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a modified version of org.jooq:jool-java-8:0.9.14.
+ *
+ * Modifications include removal of unneeded code and changes to existing code to suit jdbi's needs.
+ */
+package org.jdbi.v3.lib.internal.org_jooq.jool_java_8.v0_9_14;
