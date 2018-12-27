d9daa07c1682773a6b404185a9943a7d7f2f03f2
Marnes <marnick.leau@gmail.com>
2018-12-27 12:47:15 +0100

    license headers

--- kotlin-sqlobject/src/main/kotlin/org/jdbi/v3/sqlobject/kotlin/RegisterKotlinMappers.kt
+++ kotlin-sqlobject/src/main/kotlin/org/jdbi/v3/sqlobject/kotlin/RegisterKotlinMappers.kt
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,6 @@
 package org.jdbi.v3.sqlobject.config
 
 import org.jdbi.v3.sqlobject.kotlin.RegisterKotlinMapper
-import org.jdbi.v3.sqlobject.kotlin.RegisterKotlinMapperImpl
 import org.jdbi.v3.sqlobject.kotlin.RegisterKotlinMappersImpl
 import java.lang.annotation.ElementType
 import java.lang.annotation.Retention
