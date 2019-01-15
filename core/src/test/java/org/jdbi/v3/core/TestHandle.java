76065fca0c43e3621142524f08b4d3b7cd97fe17
Steven Schlansker <stevenschlansker@gmail.com>
2019-01-14 16:15:54 -0800

    Test transaction handler failure doesn't suppress connection close
    Fixes #1406

--- core/src/test/java/org/jdbi/v3/core/TestHandle.java
+++ core/src/test/java/org/jdbi/v3/core/TestHandle.java
@@ -14,6 +14,9 @@
 package org.jdbi.v3.core;
 
 import org.jdbi.v3.core.rule.H2DatabaseRule;
+import org.jdbi.v3.core.statement.UnableToCreateStatementException;
+import org.jdbi.v3.core.transaction.LocalTransactionHandler;
+import org.jdbi.v3.core.transaction.TransactionException;
 import org.jdbi.v3.core.transaction.TransactionIsolationLevel;
 import org.jdbi.v3.core.transaction.UnableToManipulateTransactionIsolationLevelException;
 import org.junit.Rule;
@@ -76,5 +79,63 @@ public class TestHandle {
 
         assertThatCode(() -> h.setTransactionIsolation(TransactionIsolationLevel.UNKNOWN))
             .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void testAutocommitFailDoesntLeak() {
+        final BoomHandler handler = new BoomHandler();
+        dbRule.getJdbi().setTransactionHandler(handler);
+        final Handle h = dbRule.openHandle();
+
+        assertThat(h.isClosed()).isFalse();
+
+        handler.failTest = true;
+        assertThatThrownBy(h::close)
+            .isInstanceOf(CloseException.class);
+
+        assertThat(h.isClosed()).isTrue();
+    }
+
+    @Test
+    public void testRollbackFailDoesntLeak() throws Exception {
+        final BoomHandler handler = new BoomHandler();
+        dbRule.getJdbi().setTransactionHandler(handler);
+        final Handle h = dbRule.openHandle();
+
+        assertThat(h.isClosed()).isFalse();
+
+        handler.failRollback = true;
+        assertThatThrownBy(() -> h.useTransaction(h2 -> h2.execute("insert into true")))
+            .isInstanceOf(UnableToCreateStatementException.class);
+        assertThat(h.isInTransaction())
+            .describedAs("rollback failed but handle should still be in transaction").isTrue();
+
+        assertThatThrownBy(h::close)
+            .isInstanceOf(CloseException.class);
+        assertThat(h.isClosed()).isTrue();
+        assertThat(h.getConnection().isClosed()).isTrue();
+    }
+
+    static class BoomHandler extends LocalTransactionHandler {
+        boolean failTest;
+        boolean failRollback;
+
+        @Override
+        public boolean isInTransaction(Handle handle) {
+            maybeFail(failTest);
+            return super.isInTransaction(handle);
+        }
+
+        @Override
+        public void rollback(Handle handle) {
+            maybeFail(failRollback);
+            super.rollback(handle);
+        }
+
+        private void maybeFail(boolean fail) {
+            if (fail) {
+                throw new TransactionException("connection closed");
+            }
+        }
     }
 }
