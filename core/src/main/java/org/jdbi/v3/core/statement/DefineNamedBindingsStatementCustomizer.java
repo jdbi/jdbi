15fd470a4d240b291d08061441a30ca2db6b6642
Steven Schlansker <stevenschlansker@gmail.com>
2019-01-29 16:19:46 -0800

    DefineNamedBindings: use a proxy Statement rather than inspecting for `NullArgument`

--- core/src/main/java/org/jdbi/v3/core/statement/DefineNamedBindingsStatementCustomizer.java
+++ core/src/main/java/org/jdbi/v3/core/statement/DefineNamedBindingsStatementCustomizer.java
@@ -13,20 +13,48 @@
  */
 package org.jdbi.v3.core.statement;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.Set;
 
-import org.jdbi.v3.core.argument.NullArgument;
+import org.jdbi.v3.core.argument.Argument;
+import org.jdbi.v3.core.internal.exceptions.Unchecked;
 
 class DefineNamedBindingsStatementCustomizer implements StatementCustomizer {
     @Override
-    public void beforeTemplating(PreparedStatement stmt, StatementContext ctx) throws SQLException {
+    public void beforeTemplating(PreparedStatement stmt, StatementContext ctx) {
         final Set<String> alreadyDefined = ctx.getAttributes().keySet();
         final Binding binding = ctx.getBinding();
+        final SetNullHandler handler = new SetNullHandler();
         binding.getNames().stream()
             .filter(name -> !alreadyDefined.contains(name))
             .forEach(name -> binding.findForName(name, ctx).ifPresent(
-                    a -> ctx.define(name, a instanceof NullArgument ? false : true)));
+                    a -> handler.define(name, a, ctx)));
+    }
+
+    private static class SetNullHandler implements InvocationHandler {
+        private final PreparedStatement fakeStmt = (PreparedStatement)
+                Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class<?>[] {PreparedStatement.class}, this);
+        private boolean setNull;
+        private boolean setCalled;
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) {
+            setCalled = method.getName().startsWith("set");
+            boolean argNull = args.length > 1 && args[1] == null;
+            setNull = "setNull".equals(method.getName()) || argNull;
+            return null;
+        }
+
+        void define(String name, Argument arg, StatementContext ctx) {
+            setNull = false;
+            setCalled = false;
+            Unchecked.runnable(() -> arg.apply(1, fakeStmt, ctx)).run();
+            if (setCalled) {
+                ctx.define(name, !setNull);
+            }
+        }
     }
 }
