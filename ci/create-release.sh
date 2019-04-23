21c262625b0590148deed2e15e6e92e0f2751c42
Ben Hale <bhale@pivotal.io>
2019-04-23 17:01:59 -0400

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -1,18 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -e -u
+set -euo pipefail
 
 RELEASE=$1
 SNAPSHOT=$2
 
-git branch -f release
-git checkout release
 ./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
 git commit --message "v$RELEASE Release"
 git tag -s v$RELEASE -m "v$RELEASE"
 
-git master
+git reset --hard HEAD^1
 ./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
 git add .
 git commit --message "v$SNAPSHOT Development"
