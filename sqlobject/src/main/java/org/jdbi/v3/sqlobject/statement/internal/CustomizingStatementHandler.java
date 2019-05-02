e9eee673bd48698be1034c10c1d0a60586959222
Marnes <marnick.leau@gmail.com>
2019-05-02 16:31:15 +0200

    excess exception

--- sqlobject/src/main/java/org/jdbi/v3/sqlobject/statement/internal/CustomizingStatementHandler.java
+++ sqlobject/src/main/java/org/jdbi/v3/sqlobject/statement/internal/CustomizingStatementHandler.java
@@ -138,7 +138,7 @@ abstract class CustomizingStatementHandler<StatementType extends SqlStatement<St
     }
 
     @Override
-    public Object invoke(Object target, Object[] args, HandleSupplier hs) throws Exception {
+    public Object invoke(Object target, Object[] args, HandleSupplier hs) {
         final Handle h = hs.getHandle();
         final String locatedSql = locateSql(h);
         final StatementType stmt = createStatement(h, locatedSql);
