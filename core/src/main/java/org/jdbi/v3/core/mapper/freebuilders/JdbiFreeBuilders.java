c34eb28bfbea1023bdfd691bff47f24ef8426f73
dix <william.dix@getbraintree.com>
2020-05-24 00:02:00 -0400

    Mark FreeBuilders API as Beta

--- core/src/main/java/org/jdbi/v3/core/mapper/freebuilders/JdbiFreeBuilders.java
+++ core/src/main/java/org/jdbi/v3/core/mapper/freebuilders/JdbiFreeBuilders.java
@@ -19,11 +19,13 @@ import org.jdbi.v3.core.internal.exceptions.Unchecked;
 import org.jdbi.v3.core.mapper.reflect.internal.BuilderPropertiesFactory;
 import org.jdbi.v3.core.mapper.reflect.internal.FreeBuilderPropertiesFactory;
 import org.jdbi.v3.core.mapper.reflect.internal.PojoTypes;
+import org.jdbi.v3.meta.Beta;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.function.Supplier;
 
+@Beta
 public class JdbiFreeBuilders implements JdbiConfig<JdbiFreeBuilders> {
     private ConfigRegistry registry;
 
