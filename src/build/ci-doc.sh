d949cfced6960932cf28aaa74b6bc2e97499d7f4
Steven Schlansker <stevenschlansker@gmail.com>
2021-04-02 14:24:01 -0700

    Fail on Javadoc errors during pre-release build

--- src/build/ci-doc.sh
+++ src/build/ci-doc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -xe
 
-OPTS="-DskipTests=true -Dbasepom.check.skip-all=true -Dbasepom.check.skip-javadoc=false -Dbasepom.check.fail-javadoc=false -B"
+OPTS="-DskipTests=true -Dbasepom.check.skip-all=true -Dbasepom.check.skip-javadoc=false -Dbasepom.check.fail-javadoc=true -B"
 
 exec mvn ${OPTS} verify
