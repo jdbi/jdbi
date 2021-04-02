1ee5c23465d921f3dcdae315fd64739c5fe47e32
Henning Schmiedehausen <henning@schmiedehausen.org>
2021-04-02 12:26:36 -0700

    make the jdk8/11 switch work

--- src/build/ci-test.sh
+++ src/build/ci-test.sh
@@ -7,9 +7,17 @@ OPTS="-Dbasepom.check.skip-all=true -B"
 if [ "$JDK_VERSION" = "8" ]; then
     if [ "$TRAVIS" = "true" ]; then
         # build must be done with a JDK > 8
-        jdk_switcher use openjdk11
+        OLD_JAVA_HOME=${JAVA_HOME}
+        OLD_PATH=${PATH}
+        JAVA_HOME=/usr/local/lib/jvm/openjdk11
+        PATH=${JAVA_HOME}/bin:$PATH
+        export JAVA_HOME PATH
         mvn ${OPTS} -DskipTests clean verify
-        jdk_switcher use openjdk8
+
+        # run the tests with JDK8
+        PATH=${OLD_PATH}
+        JAVA_HOME=${OLD_JAVA_HOME}
+        export JAVA_HOME PATH
         exec mvn -DargLine= surefire:test
     fi
 fi
