f9c6e26e2847cc82c879d1190e5f85df0125ffde
Henning Schmiedehausen <henning@schmiedehausen.org>
2021-06-01 14:39:16 -0700

    fix license headers correctly this time

--- kotlin/src/main/kotlin/org/jdbi/v3/core/kotlin/internal/KotlinPropertyArguments.kt
+++ kotlin/src/main/kotlin/org/jdbi/v3/core/kotlin/internal/KotlinPropertyArguments.kt
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -57,6 +57,6 @@ class KotlinPropertyArguments(obj: Any,
     override fun getNestedArgumentFinder(obj: TypedValue): NamedArgumentFinder {
         return KotlinPropertyArguments(obj.getValue())
     }
-    
+
     override fun toString() = obj.toString()
 }
