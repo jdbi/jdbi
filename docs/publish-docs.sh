ada7eb50f55b95a3aa89414d6a1fa610d8155c0b
Henning Schmiedehausen <henning@schmiedehausen.org>
2021-07-04 14:46:38 -0700

    revert accidentially commented out line

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DOC_OPTS="-Dbasepom.check.skip-all=true -Dbasepom.check.skip-javadoc=false -DskipTests"
-# mvn ${DOC_OPTS} clean install
+mvn ${DOC_OPTS} clean install
 mvn ${DOC_OPTS} -Ppublish-docs -pl :jdbi3-docs clean deploy
