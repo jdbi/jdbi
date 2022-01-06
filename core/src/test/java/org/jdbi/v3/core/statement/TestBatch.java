98655e1955dee47536f2cc17ffd30eeb2026bd1d
Steven Schlansker <stevenschlansker@gmail.com>
2022-01-06 11:26:10 -0800

    Add failing test case #1987

--- core/src/test/java/org/jdbi/v3/core/statement/TestBatch.java
+++ core/src/test/java/org/jdbi/v3/core/statement/TestBatch.java
@@ -14,11 +14,15 @@
 package org.jdbi.v3.core.statement;
 
 import java.sql.Types;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Something;
 import org.jdbi.v3.core.junit5.H2DatabaseExtension;
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -89,5 +93,97 @@ public class TestBatch {
                 assertEquals(1, counts[i]);
             }
         }
+    }
+
+    public static class BatchRecord {
+        private final UUID id, next;
+        private final long createdAt, updatedAt;
+
+        public BatchRecord(UUID id, UUID next, long createdAt, long updatedAt) {
+            this.id = id;
+            this.next = next;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+        }
+
+        public UUID getId() {
+            return id;
+        }
+
+        public UUID getNext() {
+            return next;
+        }
+
+        public long getCreatedAt() {
+            return createdAt;
+        }
+
+        public long getUpdatedAt() {
+            return updatedAt;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(createdAt, id, next, updatedAt);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            BatchRecord other = (BatchRecord) obj;
+            return createdAt == other.createdAt && Objects.equals(id, other.id)
+                    && Objects.equals(next, other.next)
+                    && updatedAt == other.updatedAt;
+        }
+
+        @Override
+        public String toString() {
+            return "BatchRecord [id=" + id + ", next=" + next + ", createdAt="
+                    + createdAt + ", updatedAt=" + updatedAt + "]";
+        }
+    }
+
+    @Test
+    public void testBatchBinding1987() {
+        Handle h = h2Extension.getSharedHandle();
+        h.execute("CREATE TABLE recs ("
+                + "id uuid PRIMARY KEY,"
+                + "next uuid,"
+                + "created_at bigint NOT NULL,"
+                + "updated_at bigint NOT NULL"
+                + ");");
+
+        UUID u1 = new UUID(0, 0);
+        UUID u2 = new UUID(1, 1);
+        List<BatchRecord> recs = Arrays.asList(
+                new BatchRecord(u1, u2, 0, 2),
+                new BatchRecord(u2, null, 0, 2));
+
+        h.useTransaction(txn -> {
+            PreparedBatch batch = txn.prepareBatch(
+                    "INSERT INTO recs (id, next, created_at, updated_at) VALUES (:id, :next, :created_at, :updated_at)");
+            for (BatchRecord rec : recs) {
+                batch.bind("id", rec.getId())
+                        .bindBySqlType("next", rec.getNext(), Types.OTHER)
+                        .bind("created_at", rec.getCreatedAt())
+                        .bind("updated_at", rec.getUpdatedAt())
+                        .add();
+            }
+            batch.execute();
+        });
+
+        assertThat(h.createQuery("SELECT * FROM recs")
+                .registerRowMapper(ConstructorMapper.factory(BatchRecord.class))
+                .mapTo(BatchRecord.class)
+                .list())
+            .containsExactlyInAnyOrderElementsOf(recs);
     }
 }
