21860e0f9dd2088435e9eb84957f20b68c87866a
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2022-05-28 10:20:05 -0700

    fix build

--- core/src/test/java/org/jdbi/v3/core/statement/StatementContextTest.java
+++ core/src/test/java/org/jdbi/v3/core/statement/StatementContextTest.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StatementContextTest {
     @Test
