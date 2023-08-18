7dd1f3682e7a380f2c78c4bbf6042dec73184b99
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-08-17 22:06:04 -0700

    add actual module files

---
+++ testing/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module jdbi3.testing {
+    requires static com.h2database;
+    requires java.logging;
+    requires static org.flywaydb.core;
+    requires static org.postgresql.jdbc;
+    requires static org.xerial.sqlitejdbc;
+    requires transitive static de.softwareforge.testing.postgres;
+    requires static jakarta.annotation;
+    requires transitive java.sql;
+    requires transitive jdbi3.core;
+    requires transitive static junit;
+    requires transitive static org.junit.jupiter.api;
+    requires transitive static otj.pg.embedded;
+    exports org.jdbi.v3.testing;
+    exports org.jdbi.v3.testing.junit5;
+}
