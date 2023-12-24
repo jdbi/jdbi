8cc2d2fa93adb60bcd07259594385f7fb2ffacda
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-12-24 15:58:22 -0800

    support customization on call operations

--- core/src/main/java/org/jdbi/v3/core/statement/Call.java
+++ core/src/main/java/org/jdbi/v3/core/statement/Call.java
@@ -17,6 +17,7 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
@@ -153,6 +154,57 @@ public class Call extends SqlStatement<Call> {
             }
         }
         return resultComputer.apply(out);
+    }
+
+    /**
+     * Specify the fetch size for the call. This should cause the results to be
+     * fetched from the underlying RDBMS in groups of rows equal to the number passed.
+     * This is useful for doing chunked streaming of results when exhausting memory
+     * could be a problem.
+     *
+     * @param fetchSize the number of rows to fetch in a bunch
+     *
+     * @return the modified call
+     */
+    public Call setFetchSize(final int fetchSize) {
+        return addCustomizer(StatementCustomizers.fetchSize(fetchSize));
+    }
+
+    /**
+     * Specify the maximum number of rows the call is to return. This uses the underlying JDBC
+     * {@link Statement#setMaxRows(int)}}.
+     *
+     * @param maxRows maximum number of rows to return
+     *
+     * @return modified call
+     */
+    public Call setMaxRows(final int maxRows) {
+        return addCustomizer(StatementCustomizers.maxRows(maxRows));
+    }
+
+    /**
+     * Specify the maximum field size in the result set. This uses the underlying JDBC
+     * {@link Statement#setMaxFieldSize(int)}
+     *
+     * @param maxFields maximum field size
+     *
+     * @return modified call
+     */
+    public Call setMaxFieldSize(final int maxFields) {
+        return addCustomizer(StatementCustomizers.maxFieldSize(maxFields));
+    }
+
+    /**
+     * Specify that the result set should be concurrent updatable.
+     *
+     * This will allow the update methods to be called on the result set produced by this
+     * Call.
+     *
+     * @return the modified call
+     */
+    public Call concurrentUpdatable() {
+        getContext().setConcurrentUpdatable(true);
+        return this;
     }
 
     // TODO tostring?
