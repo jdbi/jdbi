7a6372ec634475f3c8ef8b07dd75a4a230dbf413
Henning P. Schmiedehausen <henning@appfolio.com>
2024-01-09 11:18:49 -0800

    Roll lombok module into e2e
    Remove the separate lombok testing module in favor of e2e testing.

--- e2e/src/test/java/org/jdbi/v3/e2e/lombok/LombokReflectionTest.java
+++ e2e/src/test/java/org/jdbi/v3/e2e/lombok/LombokReflectionTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jdbi.v3.lombok;
+package org.jdbi.v3.e2e.lombok;
 
 import java.util.Collections;
 import java.util.List;
