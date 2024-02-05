23e0e363f2da901e36aef10541968d7343bf819a
Stoyan Tsonev <st88450@googlemail.com>
2024-02-05 23:50:23 +0100

    test returning DB2 blob as byte[] added

--- testcontainers/src/test/java/org/jdbi/v3/testing/junit5/tc/DB2JdbiTestContainersExtensionTest.java
+++ testcontainers/src/test/java/org/jdbi/v3/testing/junit5/tc/DB2JdbiTestContainersExtensionTest.java
@@ -1,11 +1,14 @@
 package org.jdbi.v3.testing.junit5.tc;
 
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.Db2Container;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("slow")
 @Testcontainers
@@ -19,5 +22,22 @@ public class DB2JdbiTestContainersExtensionTest extends AbstractJdbiTestcontaine
     @Override
     JdbcDatabaseContainer<?> getDbContainer() {
         return dbContainer;
+    }
+
+    @Test
+    public void testByteaArrayMultiRows() {
+        assertThat(getUserNameAsBlobById(1)).isEqualTo("Alice".getBytes());
+        assertThat(getUserNameAsBlobById(2)).isEqualTo("Bob".getBytes());
+    }
+
+    private byte[] getUserNameAsBlobById(Integer id) {
+        var result = extension.getJdbi().withHandle(handle ->
+            handle.createQuery("SELECT BLOB(name) FROM users WHERE id = :id")
+                .bind("id", id)
+                .mapTo(byte[].class)
+                .first()
+        );
+        assertThat(result).isInstanceOf(byte[].class);
+        return result;
     }
 }
