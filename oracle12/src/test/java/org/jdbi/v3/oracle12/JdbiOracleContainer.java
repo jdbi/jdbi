d138179638621812e048bbf7d5e6c42cdf660739
Steven Schlansker <stevenschlansker@gmail.com>
2024-05-06 09:04:53 -0700

    fix

--- oracle12/src/test/java/org/jdbi/v3/oracle12/JdbiOracleContainer.java
+++ oracle12/src/test/java/org/jdbi/v3/oracle12/JdbiOracleContainer.java
@@ -4,9 +4,11 @@ import java.time.Duration;
 
 import org.testcontainers.containers.OracleContainer;
 
-public class JdbiOracleContainer {
+public final class JdbiOracleContainer {
     private static final String CONTAINER_VERSION =
             "gvenzl/oracle-xe:" + System.getProperty("oracle.container.version", "slim-faststart");
+
+    private JdbiOracleContainer() {}
 
     @SuppressWarnings("resource")
     public static OracleContainer create() {
