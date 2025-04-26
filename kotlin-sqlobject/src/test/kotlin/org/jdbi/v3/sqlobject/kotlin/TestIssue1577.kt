1bb3e7ee043efd8b44bf69168563bb5ab98d2a79
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-04-25 17:12:06 -0700

    Validate that #2770 really fixed #1577
    Add tests for all types of extension methods, all tests fail before
    merging #2770, now they pass.

---
+++ kotlin-sqlobject/src/test/kotlin/org/jdbi/v3/sqlobject/kotlin/TestIssue1577.kt
@@ -0,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdbi.v3.sqlobject.kotlin
+
+import de.softwareforge.testing.postgres.junit5.EmbeddedPgExtension
+import de.softwareforge.testing.postgres.junit5.MultiDatabaseBuilder
+import org.assertj.core.api.Assertions.assertThat
+import org.jdbi.v3.core.kotlin.KotlinMapper
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.sqlobject.SqlObjectPlugin
+import org.jdbi.v3.sqlobject.customizer.Bind
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import org.jdbi.v3.testing.junit5.JdbiExtension
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class TestIssue1577 {
+
+    companion object {
+        @RegisterExtension
+        val pg: EmbeddedPgExtension = MultiDatabaseBuilder.instanceWithDefaults().build()
+    }
+
+    private lateinit var onDemandDao: DataDao
+
+    @RegisterExtension
+    @JvmField
+    val pgExtension: JdbiExtension = JdbiExtension.postgres(pg)
+        .withInitializer { ds, handle ->
+            handle.execute("CREATE TABLE data (id serial primary key, comma varchar(50))")
+            handle.execute("INSERT INTO data (comma) VALUES ('one,two,three,four')")
+        }
+        .withPlugins(SqlObjectPlugin(), KotlinPlugin())
+
+    @BeforeEach
+    fun setup() {
+        val jdbi = pgExtension.jdbi
+        jdbi.registerRowMapper(KotlinMapper(Data::class))
+
+        this.onDemandDao = jdbi.onDemand(DataDao::class.java)
+    }
+
+    @Test
+    fun testOnDemand() {
+        val data = onDemandDao.getData(1)
+        assertThat(data.comma).isNotEmpty()
+    }
+
+    @Test
+    fun testAttach() {
+        val handle = pgExtension.sharedHandle
+        val localDao = handle.attach(DataDao::class)
+        val data = localDao.getData(1)
+        assertThat(data.comma).isNotEmpty()
+    }
+
+    @Test
+    fun testExtension() {
+        val data = pgExtension.jdbi.withExtension<Data, DataDao, RuntimeException>(DataDao::class.java, { dao -> dao.getData(1) })
+        assertThat(data.comma).isNotEmpty()
+    }
+
+    data class Data(val comma: String)
+
+    interface BaseDao<T> {
+        @SqlQuery
+        fun getData(id: Int): T
+    }
+
+    interface DataDao : BaseDao<Data> {
+        @SqlQuery("SELECT comma FROM data where id = :id")
+        override fun getData(@Bind("id") id: Int): Data
+    }
+}
