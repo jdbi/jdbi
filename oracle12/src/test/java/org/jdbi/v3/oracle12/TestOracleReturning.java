bc158d10404659dfbcc29f785f445c14de35e664
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-07-05 20:56:45 -0700

    Add Oracle-free to the build
    - move all oracle tests to use oracle-free (which has an arm64 version)
    - add tests for oracle-free to testcontainers

--- oracle12/src/test/java/org/jdbi/v3/oracle12/TestOracleReturning.java
+++ oracle12/src/test/java/org/jdbi/v3/oracle12/TestOracleReturning.java
@@ -25,11 +25,10 @@ import org.jdbi.v3.testing.junit5.tc.JdbiTestcontainersExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.oracle.OracleContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jdbi.v3.oracle12.OracleReturning.returnParameters;
@@ -40,10 +39,9 @@ import static org.jdbi.v3.oracle12.OracleReturning.returningDml;
  */
 @Tag("slow")
 @Testcontainers
-@EnabledOnOs(architectures = {"x86_64", "amd64"})
 public class TestOracleReturning {
 
-    static final String CONTAINER_VERSION = "gvenzl/oracle-xe:" + System.getProperty("oracle.container.version", "slim-faststart");
+    static final String CONTAINER_VERSION = "gvenzl/oracle-free:" + System.getProperty("oracle.container.version", "slim-faststart");
 
     @Container
     static OracleContainer oc = new OracleContainer(CONTAINER_VERSION);
